6a11940623aea4f82bf46879e89e5925b30154db
tripdubroot <ian.philpot@microsoft.com>
2017-04-05 13:50:23 -0400

    New devenv script for mac removes docker cli

---
+++ scripts/devenv_mac.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+set -x
+
+docker build --pull -t acs-engine .
+
+docker run -it \
+	--privileged \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v `pwd`:/gopath/src/github.com/Azure/acs-engine \
+	-w /gopath/src/github.com/Azure/acs-engine \
+		acs-engine /bin/bash
