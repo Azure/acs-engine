a6daa7273f720676b53e26fcbe85c48bdf090540
Sertaç Özercan <sozercan@gmail.com>
2017-04-06 14:41:08 -0700

    ☁️  mounting .azure
    Mounting .azure so user is logged in to az inside the container

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -9,6 +9,7 @@ docker run -it \
 	--privileged \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v `pwd`:/gopath/src/github.com/Azure/acs-engine \
+	-v ~/.azure:/root/.azure \
 	-w /gopath/src/github.com/Azure/acs-engine \
 		acs-engine /bin/bash
 
