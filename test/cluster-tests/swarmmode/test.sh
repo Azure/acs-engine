907440d9a06efabacb15e49ef59ead7a29911299
dmitsh <dmitsh@microsoft.com>
2017-05-12 14:58:29 -0700

    fixed validation test for swarmmode (#612)
    * fixed validation test for swarmmode
    
    * typo

--- test/cluster-tests/swarmmode/test.sh
+++ test/cluster-tests/swarmmode/test.sh
@@ -4,44 +4,57 @@ set -x
 set -e
 set -u
 
-remote_exec="ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -p2200 azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com"
+ssh_args="-i ${SSH_KEY} -o StrictHostKeyChecking=no -p2200 azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com"
 
 function teardown {
-  ${remote_exec} docker service rm nginx || true
+  ssh ${ssh_args} docker service rm nginx || true
   sleep 10
-  ${remote_exec} docker network rm network || true
+  ssh ${ssh_args} docker network rm network || true
 }
 
 trap teardown EXIT
 
-# TODO: investigate if this is really needed - replace with deterministic wait or retry loop
-sleep 60
+sleep 30
 
-${remote_exec} docker network create \
-	--driver overlay \
-	--subnet 10.0.9.0/24 \
-	--opt encrypted \
-	network
+wait=10
+count=12
+args="${ssh_args} docker network create --driver overlay --subnet 10.0.9.0/24 --opt encrypted network"
+while (( $count > 0 )); do
+  ret=$(ssh $args || echo "ssh_error")
+  if [[ "$ret" != "ssh_error" ]]; then break; fi
+  sleep $wait
+  count=$((count-1))
+done
+if [[ "$ret" == "ssh_error" ]]; then
+  echo "gave up waiting for network to be created"
+  exit -1
+fi
 
-${remote_exec} docker service create \
-	--replicas 3 \
-	--name nginx \
-	--network network \
-	--publish 80:80 \
-	nginx
+wait=5
+count=12
+args="${ssh_args} docker service create --replicas 3 --name nginx --network network --publish 80:80 nginx"
+while (( $count > 0 )); do
+  ret=$(ssh $args || echo "ssh_error")
+  if [[ "$ret" != "ssh_error" ]]; then break; fi
+  sleep $wait
+  count=$((count-1))
+done
+if [[ "$ret" == "ssh_error" ]]; then
+  echo "gave up waiting for service to be created"
+  exit -1
+fi
 
 sleep 10
 
 wait=5
 count=12
-success="n"
 while (( $count > 0 )); do
   ret=$(curl --fail "http://${INSTANCE_NAME}0.${LOCATION}.cloudapp.azure.com:80/" || echo "curl_error")
-  if [[ ! $ret =~ .*curl_error.* ]]; then
-    success="y"
-    break
-  fi
+  if [[ "$ret" != "curl_error" ]]; then break; fi
+  sleep $wait
+  count=$((count-1))
 done
-if [[ "${success}" != "y" ]]; then
-  echo "gave up waiting for service to be externally reachable"; exit -1
+if [[ "$ret" == "curl_error" ]]; then
+  echo "gave up waiting for service to be externally reachable"
+  exit -1
 fi
