cecec29372e44eef4d3351cd02c4c6bec1977710
Jingtao Ren <jiren@microsoft.com>
2017-05-24 10:53:37 -0700

    add DeleteResourceGroup function in ArmHelpers

--- pkg/armhelpers/groupsclient.go
+++ pkg/armhelpers/groupsclient.go
@@ -1,6 +1,8 @@
 package armhelpers
 
 import (
+	"net/http"
+
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
 	"github.com/prometheus/common/log"
 )
@@ -17,4 +19,17 @@ func (az *AzureClient) EnsureResourceGroup(name, location string) (resourceGroup
 	}
 
 	return &response, nil
+}
+
+// DeleteResourceGroup delete the named resource group
+func (az *AzureClient) DeleteResourceGroup(name string, cancel chan struct{}) error {
+	resCh, errCh := az.groupsClient.Delete(name, cancel)
+	res := <-resCh
+	err := <-errCh
+	// When resourceGroup not exists, we will get 404
+	// Explictly set the error to nil for this scenario
+	if res.Response != nil && res.StatusCode == http.StatusNotFound {
+		return nil
+	}
+	return err
 }
