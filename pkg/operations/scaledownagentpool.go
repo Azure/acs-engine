aa21c4ff3643edf8dc267d7d617dd30c4dc61f16
Jack <jaquincy@microsoft.com>
2017-05-26 14:51:33 -0700

    changing operation to have a list of vm names
    changing operation to have a list of vm names, as we are polling CRP for
    that info earlier in the flow and it greatly simplifies the logic. Only
    drawback is failure handling is less elegant, but we shoudl be able to
    handle those with some caching of things to delete/retries

--- pkg/operations/scaledownagentpool.go
+++ pkg/operations/scaledownagentpool.go
@@ -2,71 +2,31 @@ package operations
 
 import (
 	"container/list"
-	"fmt"
 
-	"github.com/Azure/acs-engine/pkg/api"
 	"github.com/Azure/acs-engine/pkg/armhelpers"
 )
 
-var vmNameFormats = make(map[api.OrchestratorType]map[api.OSType]string)
-
-const (
-	//These formats assume the arguments to Sprintf are agent pool index, name suffix, vm index in that order
-	windowsAgentVMFormat           = "%.5[2]sacs9%02[1]d%[3]d"
-	k8sLinuxAgentVMFormat          = "k8s-agent%d-%s-%d"
-	dcosLinuxAgentVMFormat         = "dcos-agent%d-%s-%d"
-	mesosLinuxAgentVMFormat        = "mesos-agent%d-%s-%d"
-	swarmLinuxAgentVMFormat        = "swarm-agent%d-%s-%d"
-	swarmModeLinuxAgentVMFormat    = "swarmm-agent%d-%s-%d"
-	k8sClassicLinuxAgentVMFormat   = "k8s-agent-%[2]s-%[3]d"
-	dcosClassicLinuxAgentVMFormat  = "dcos-agent-%[2]s-%[3]d"
-	swarmClassicLinuxAgentVMFormat = "swarm-agent-%[2]s-%[3]d"
-)
-
-func init() {
-	vmNameFormats[api.Kubernetes] = make(map[api.OSType]string)
-	vmNameFormats[api.Kubernetes][api.Linux] = k8sLinuxAgentVMFormat
-	vmNameFormats[api.Kubernetes][api.Windows] = windowsAgentVMFormat
-	vmNameFormats[api.DCOS] = make(map[api.OSType]string)
-	vmNameFormats[api.DCOS][api.Linux] = dcosLinuxAgentVMFormat
-	// Mesos is deprecated in favor of DCOS never supported VMAS agents so we shouldn't need a name format
-	// This line is just to prevent a nil dereference
-	vmNameFormats[api.Mesos] = make(map[api.OSType]string)
-	vmNameFormats[api.Swarm] = make(map[api.OSType]string)
-	vmNameFormats[api.Swarm][api.Linux] = swarmLinuxAgentVMFormat
-	vmNameFormats[api.Swarm][api.Windows] = windowsAgentVMFormat
-	vmNameFormats[api.SwarmMode] = make(map[api.OSType]string)
-	vmNameFormats[api.SwarmMode][api.Linux] = swarmModeLinuxAgentVMFormat
-	vmNameFormats[api.SwarmMode][api.Windows] = windowsAgentVMFormat
-}
-
 //VMScalingErrorDetails give the index in the agent pool that failed and the accompanying error
 type VMScalingErrorDetails struct {
-	Index int
+	Name  string
 	Error error
 }
 
-// ScaleDownVMASAgentPool removes the vms at the specified indexes from the specified agent pool. Returns a list with details on each failure.
+// ScaleDownVMs removes the vms in the provided list. Returns a list with details on each failure.
 // all items in the list will always be of type *VMScalingErrorDetails
-func ScaleDownVMASAgentPool(az armhelpers.ACSEngineClient, clusterSuffix, resourceGroup string, agentPoolIndex int,
-	orchestratorType api.OrchestratorType, osType api.OSType, indexesToRemove ...int) *list.List {
-	numVmsToDelete := len(indexesToRemove)
+func ScaleDownVMs(az armhelpers.ACSEngineClient, resourceGroup string, vmNames ...string) *list.List {
+	numVmsToDelete := len(vmNames)
 	errChan := make(chan *VMScalingErrorDetails, numVmsToDelete)
 	defer close(errChan)
-	for _, vmIndex := range indexesToRemove {
-		go func(vmIndex int) {
-			vmName, err := getVMName(orchestratorType, osType, agentPoolIndex, vmIndex, clusterSuffix)
+	for _, vmName := range vmNames {
+		go func(vmName string) {
+			err := CleanDeleteVirtualMachine(az, resourceGroup, vmName)
 			if err != nil {
-				errChan <- &VMScalingErrorDetails{Index: vmIndex, Error: err}
-				return
-			}
-			err = CleanDeleteVirtualMachine(az, resourceGroup, vmName)
-			if err != nil {
-				errChan <- &VMScalingErrorDetails{Index: vmIndex, Error: err}
+				errChan <- &VMScalingErrorDetails{Name: vmName, Error: err}
 				return
 			}
 			errChan <- nil
-		}(vmIndex)
+		}(vmName)
 	}
 	failedVMDeletions := &list.List{}
 	for i := 0; i < numVmsToDelete; i++ {
@@ -79,12 +39,4 @@ func ScaleDownVMASAgentPool(az armhelpers.ACSEngineClient, clusterSuffix, resour
 		return failedVMDeletions
 	}
 	return nil
-}
-
-func getVMName(orchestratorType api.OrchestratorType, osType api.OSType, agentpoolIndex, vmIndex int, clusterSuffix string) (string, error) {
-	format, ok := vmNameFormats[orchestratorType][osType]
-	if !ok {
-		return "", fmt.Errorf("Unable to find vm name format for Orchestrator %s and OS %s", orchestratorType, osType)
-	}
-	return fmt.Sprintf(format, agentpoolIndex, clusterSuffix, vmIndex), nil
 }
--- pkg/operations/scaledownagentpool_test.go
+++ pkg/operations/scaledownagentpool_test.go
@@ -3,77 +3,22 @@ package operations
 import (
 	"testing"
 
-	"github.com/Azure/acs-engine/pkg/api"
 	. "github.com/onsi/gomega"
 )
 
-func Test_getVMName(t *testing.T) {
+func Test_ScaleDownVMs_ErrorPath(t *testing.T) {
 	RegisterTestingT(t)
-	suffix := "F8EADCCF"
-	agentIndex := 12
-	shortPoolIndex := 3
-	longPoolIndex := 45
-
-	result, err := getVMName(api.Kubernetes, api.Linux, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("k8s-agent3-F8EADCCF-12"))
-
-	result, err = getVMName(api.Kubernetes, api.Windows, longPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs94512"))
-
-	result, err = getVMName(api.Kubernetes, api.Windows, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs90312"))
-
-	result, err = getVMName(api.DCOS, api.Linux, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("dcos-agent3-F8EADCCF-12"))
-
-	result, err = getVMName(api.DCOS, api.Windows, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(Not(BeNil()))
-	Expect(result).To(Equal(""))
-
-	result, err = getVMName(api.Swarm, api.Linux, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("swarm-agent3-F8EADCCF-12"))
-
-	result, err = getVMName(api.Swarm, api.Windows, longPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs94512"))
-
-	result, err = getVMName(api.Swarm, api.Windows, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs90312"))
-
-	result, err = getVMName(api.SwarmMode, api.Linux, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("swarmm-agent3-F8EADCCF-12"))
-
-	result, err = getVMName(api.SwarmMode, api.Windows, longPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs94512"))
-
-	result, err = getVMName(api.SwarmMode, api.Windows, shortPoolIndex, agentIndex, suffix)
-	Expect(err).To(BeNil())
-	Expect(result).To(Equal("F8EADacs90312"))
-}
-
-func Test_ScaleDownVMASAgentPool_ErrorPath(t *testing.T) {
-	RegisterTestingT(t)
-	errs := ScaleDownVMASAgentPool(&failingMockClient{}, "F8EADCCF", "rg", 1, api.Kubernetes, api.Linux,
-		2, 3, 4, 6)
+	errs := ScaleDownVMs(&failingMockClient{}, "rg", "vm1", "vm2", "vm3", "vm5")
 	Expect(errs.Len()).To(Equal(4))
 	for e := errs.Front(); e != nil; e = e.Next() {
 		output := e.Value.(*VMScalingErrorDetails)
-		Expect(output.Index).To(Not(Equal(0)))
+		Expect(output.Name).To(ContainSubstring("vm"))
 		Expect(output.Error).To(Not(BeNil()))
 	}
 }
 
-func Test_ScaleDownVMASAgentPool_HappyPath(t *testing.T) {
+func Test_ScaleDownVMs_HappyPath(t *testing.T) {
 	RegisterTestingT(t)
-	errs := ScaleDownVMASAgentPool(&mockClient{}, "F8EADCCF", "rg", 1, api.Kubernetes, api.Linux,
-		2, 3, 4, 6)
+	errs := ScaleDownVMs(&mockClient{}, "rg", "k8s-agent-F8EADCCF-0", "k8s-agent-F8EADCCF-3", "k8s-agent-F8EADCCF-2", "k8s-agent-F8EADCCF-4")
 	Expect(errs).To(BeNil())
 }
