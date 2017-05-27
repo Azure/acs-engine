2c184adaf303b64486948f8e977d3d2a73f785a0
Rob Bagby <rob@robbagby.com>
2017-05-26 17:36:43 -0700

    Updated hello-world-k8s.

--- extensions/hello-world-k8s/v1/hello-world-k8s.sh
+++ extensions/hello-world-k8s/v1/hello-world-k8s.sh
@@ -9,9 +9,9 @@ echo $(date) " - Starting Script"
 # Deploy container
 echo $(date) " - Deploying hello-world container"
 
-kubectl run hello-world --image=hello-world
+kubectl run hello-world --quiet --image=busybox --restart=OnFailure -- echo "Hello Kubernetes!"
 
-echo $(date) " - run kubectl get pods to list the pods"
+echo $(date) " - run kubectl get pods --show-all to list the pods"
 echo $(date) " - run kubectl logs (passing the pod name gathered from kubectl get pods)"
 echo $(date) " - Script complete"
 
