5b186dd38c898fe9f7c37c94d7a232e8005c1bb5
dmitsh <dmitsh@microsoft.com>
2017-06-12 16:36:25 -0700

    added check for node count in DCOS (#757)

--- test/acs-engine-test/main.go
+++ test/acs-engine-test/main.go
@@ -155,17 +155,15 @@ func (m *TestManager) testRun(d Deployment, index, attempt int, timeout time.Dur
 			}
 			env = append(env, fmt.Sprintf("EXPECTED_ORCHESTRATOR_VERSION=%s", strings.TrimSpace(string(out))))
 
-			if orchestrator == "kubernetes" {
-				cmd = exec.Command("test/step.sh", "get_node_count")
-				cmd.Env = env
-				out, err = cmd.Output()
-				if err != nil {
-					wrileLog(logFile, "Error [%s:%s] %v", "get_node_count", resourceGroup, err)
-					success = false
-					break
-				}
-				env = append(env, fmt.Sprintf("EXPECTED_NODE_COUNT=%s", strings.TrimSpace(string(out))))
+			cmd = exec.Command("test/step.sh", "get_node_count")
+			cmd.Env = env
+			out, err = cmd.Output()
+			if err != nil {
+				wrileLog(logFile, "Error [%s:%s] %v", "get_node_count", resourceGroup, err)
+				success = false
+				break
 			}
+			env = append(env, fmt.Sprintf("EXPECTED_NODE_COUNT=%s", strings.TrimSpace(string(out))))
 		}
 	}
 	// clean up
