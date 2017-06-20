23d2a03bb24e7af7fa87fb3c5c32155a08bb370b
Jiangtian Li <JiangtianLi@users.noreply.github.com>
2017-06-20 13:17:41 -0700

    Add ListNextResults to deployment operations (#813)

--- pkg/armhelpers/deploymentOperations.go
+++ pkg/armhelpers/deploymentOperations.go
@@ -8,3 +8,8 @@ import (
 func (az *AzureClient) ListDeploymentOperations(resourceGroupName string, deploymentName string, top *int32) (result resources.DeploymentOperationsListResult, err error) {
 	return az.deploymentOperationsClient.List(resourceGroupName, deploymentName, top)
 }
+
+// ListDeploymentOperationsNextResults retrieves the next set of results, if any.
+func (az *AzureClient) ListDeploymentOperationsNextResults(lastResults resources.DeploymentOperationsListResult) (result resources.DeploymentOperationsListResult, err error) {
+	return az.deploymentOperationsClient.ListNextResults(lastResults)
+}
