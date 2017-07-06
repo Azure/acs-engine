8e86abcfde82bd124134f0e7d0ccbafc1a10d5c6
Markus Padourek <markus@hufsy.com>
2017-07-06 15:30:36 +0200

    Smaller fixes,

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -99,10 +99,8 @@ var kubernetesAritfacts = map[string]string{
 var kubernetesAddonYamls = map[string]string{
 	"MASTER_ADDON_HEAPSTER_DEPLOYMENT_B64_GZIP_STR":             "kubernetesmasteraddons-heapster-deployment.yaml",
 	"MASTER_ADDON_HEAPSTER_SERVICE_B64_GZIP_STR":                "kubernetesmasteraddons-heapster-service.yaml",
-	"MASTER_ADDON_HEAPSTER_ROLEBINDING_B64_GZIP_STR":            "kubernetesmasteraddons-heapster-rolebinding.yaml",
 	"MASTER_ADDON_KUBE_DNS_DEPLOYMENT_B64_GZIP_STR":             "kubernetesmasteraddons-kube-dns-deployment.yaml",
 	"MASTER_ADDON_KUBE_DNS_SERVICE_B64_GZIP_STR":                "kubernetesmasteraddons-kube-dns-service.yaml",
-	"MASTER_ADDON_KUBE_DNS_ROLEBINDING_B64_GZIP_STR":            "kubernetesmasteraddons-kube-dns-rolebinding.yaml",
 	"MASTER_ADDON_KUBE_PROXY_DAEMONSET_B64_GZIP_STR":            "kubernetesmasteraddons-kube-proxy-daemonset.yaml",
 	"MASTER_ADDON_KUBERNETES_DASHBOARD_DEPLOYMENT_B64_GZIP_STR": "kubernetesmasteraddons-kubernetes-dashboard-deployment.yaml",
 	"MASTER_ADDON_KUBERNETES_DASHBOARD_SERVICE_B64_GZIP_STR":    "kubernetesmasteraddons-kubernetes-dashboard-service.yaml",
