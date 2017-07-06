367d978b3cf791be4aa785c1114cc1f51e4894f3
Markus Padourek <markus@hufsy.com>
2017-07-06 10:06:00 +0200

    Add kube-dns rolebindings.

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -102,6 +102,7 @@ var kubernetesAddonYamls = map[string]string{
 	"MASTER_ADDON_HEAPSTER_ROLEBINDING_B64_GZIP_STR":            "kubernetesmasteraddons-heapster-rolebinding.yaml",
 	"MASTER_ADDON_KUBE_DNS_DEPLOYMENT_B64_GZIP_STR":             "kubernetesmasteraddons-kube-dns-deployment.yaml",
 	"MASTER_ADDON_KUBE_DNS_SERVICE_B64_GZIP_STR":                "kubernetesmasteraddons-kube-dns-service.yaml",
+	"MASTER_ADDON_KUBE_DNS_ROLEBINDING_B64_GZIP_STR":            "kubernetesmasteraddons-kube-dns-rolebinding.yaml",
 	"MASTER_ADDON_KUBE_PROXY_DAEMONSET_B64_GZIP_STR":            "kubernetesmasteraddons-kube-proxy-daemonset.yaml",
 	"MASTER_ADDON_KUBERNETES_DASHBOARD_DEPLOYMENT_B64_GZIP_STR": "kubernetesmasteraddons-kubernetes-dashboard-deployment.yaml",
 	"MASTER_ADDON_KUBERNETES_DASHBOARD_SERVICE_B64_GZIP_STR":    "kubernetesmasteraddons-kubernetes-dashboard-service.yaml",
