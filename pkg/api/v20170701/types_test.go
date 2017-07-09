efee998440a8be3eff7e099f5db6f96f9bb870b0
Jingtao Ren <rjtsdl@gmail.com>
2017-07-08 20:34:33 -0700

    populate default value in unmarshal (#939)
    * populate default mastercount to 1 in unmarshalJSON
    
    * populate default value for agentpool count and osType for v2017-01-31
    
    * add unmarshal function for 2016-09-30 and 2016-03-30
    
    * add unmarshal function for 2017-07-01, with TODO comments

--- pkg/api/v20170701/types_test.go
+++ pkg/api/v20170701/types_test.go
@@ -1,24 +1,9 @@
-package v20160930
+package v20170701
 
 import (
 	"encoding/json"
 	"testing"
 )
-
-func TestIsDCOS(t *testing.T) {
-	dCOSProfile := &OrchestratorProfile{
-		OrchestratorType: "DCOS",
-	}
-	if !dCOSProfile.IsDCOS() {
-		t.Fatalf("unable to detect DCOS orchestrator profile from OrchestratorType=%s", dCOSProfile.OrchestratorType)
-	}
-	kubernetesProfile := &OrchestratorProfile{
-		OrchestratorType: "Kubernetes",
-	}
-	if kubernetesProfile.IsDCOS() {
-		t.Fatalf("unexpectedly detected DCOS orchestrator profile from OrchestratorType=%s", kubernetesProfile.OrchestratorType)
-	}
-}
 
 func TestMasterProfile(t *testing.T) {
 	MasterProfileText := "{\"count\" : 0}"
