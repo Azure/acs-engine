5c86bfa35d35ab8dd13b7ba0ca824b2d384bdd38
Dmitry Shmulevich <dmitsh@microsoft.com>
2017-07-19 15:00:43 -0700

    added 'cooling' period between subsequent restarts

--- parts/kubelet-restart.sh
+++ parts/kubelet-restart.sh
@@ -4,7 +4,22 @@ if [ ! -e /opt/azure/containers/runcmd.complete ]; then
   exit 0
 fi
 
-ready=$(kubectl --kubeconfig /var/lib/kubelet/kubeconfig get nodes/$(hostname) -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
-if [ "$ready" = "Unknown" ]; then
+READY=$(kubectl --kubeconfig /var/lib/kubelet/kubeconfig get nodes/$(hostname) -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
+if [ "$ready" != "Unknown" ]; then
+  exit 0
+fi
+
+# Setting "cooling" interval between subsequent restarts to 30 minutes (1800 sec.)
+COOLING=1800
+FNAME=/var/run/kubelet-last-restart
+if [ ! -e $FNAME ]; then
+  touch $FNAME
+  SINCE_LAST=${COOLING}
+else
+  SINCE_LAST=$(($(date +%s) - $(stat -c %X $FNAME)))
+fi
+
+if [ ${SINCE_LAST} -ge ${COOLING} ]; then
   systemctl restart kubelet
+  touch $FNAME
 fi
