cb77b5835c4fc1f3332821cea2719d7c78be81ad
uday31in <uday.pandya@microsoft.com>
2017-07-24 22:58:34 +0100

    Fixing comment description for IsNotUbuntu

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -384,7 +384,7 @@ func (a *AgentPoolProfile) IsLinux() bool {
 	return a.OSType == Linux
 }
 
-// IsUbuntu returns true if OSImagePublisher is Canonical
+// IsUbuntu returns true if OSImagePublisher anything but Canonical
 func (a *AgentPoolProfile) IsNotUbuntu() bool {
 	return a.OsImagePublisher == "CoreOS"
 }
