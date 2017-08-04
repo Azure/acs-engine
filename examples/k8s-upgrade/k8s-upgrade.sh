ed928a8d6d71cd052a867583001d7a10c61d6e62
Dmitry Shmulevich <dmitsh@microsoft.com>
2017-08-04 09:26:10 -0700

    set azure authentication

--- examples/k8s-upgrade/k8s-upgrade.sh
+++ examples/k8s-upgrade/k8s-upgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+set -e
+
 export ACSENGINE_EXPERIMENTAL_FEATURES=1
 
 OUTPUT="_output/${INSTANCE_NAME}"
@@ -15,4 +18,11 @@ cat > $K8S_UPGRADE_CONF <<END
 }
 END
 
-./bin/acs-engine upgrade --subscription-id ${SUBSCRIPTION_ID} --deployment-dir ${OUTPUT} --resource-group ${RESOURCE_GROUP} --upgrademodel-file $K8S_UPGRADE_CONF
+./bin/acs-engine upgrade \
+  --subscription-id ${SUBSCRIPTION_ID} \
+  --deployment-dir ${OUTPUT} \
+  --resource-group ${RESOURCE_GROUP} \
+  --upgrademodel-file $K8S_UPGRADE_CONF \
+  --auth-method client_secret \
+  --client-id ${SERVICE_PRINCIPAL_CLIENT_ID} \
+  --client-secret ${SERVICE_PRINCIPAL_CLIENT_SECRET}
