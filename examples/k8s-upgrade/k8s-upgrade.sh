88471a18ee9ac623ed501218b17d35bb349938d7
Dmitry Shmulevich <dmitsh@microsoft.com>
2017-08-03 18:36:06 -0700

    adding test files for k8s upgrade

---
+++ examples/k8s-upgrade/k8s-upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export ACSENGINE_EXPERIMENTAL_FEATURES=1
+
+OUTPUT="_output/${INSTANCE_NAME}"
+K8S_UPGRADE_CONF="$OUTPUT/k8sUpgrade.json"
+
+cat > $K8S_UPGRADE_CONF <<END
+{
+  "apiVersion": "vlabs",
+  "orchestratorProfile": {
+      "orchestratorType": "Kubernetes",
+      "orchestratorVersion": "1.6.2"
+    }
+}
+END
+
+./bin/acs-engine upgrade --subscription-id ${SUBSCRIPTION_ID} --deployment-dir ${OUTPUT} --resource-group ${RESOURCE_GROUP} --upgrademodel-file $K8S_UPGRADE_CONF
