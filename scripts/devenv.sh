cf834c38db0c85a4fed5ce96610fb287e9812013
Sean Knox <sean.knox@microsoft.com>
2017-08-07 17:30:46 -0700

    fix(docker): don't mount repo, rebuild image instead; fix image tag

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -3,12 +3,11 @@
 set -eu -o pipefail
 set -x
 
-docker build --pull -t acs-engine .
+docker build -t microsoft/acs-engine .
 
 docker run -it \
 	--privileged \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v `pwd`:/gopath/src/github.com/Azure/acs-engine \
 	-v ~/.azure:/root/.azure \
 	-w /gopath/src/github.com/Azure/acs-engine \
 		acs-engine /bin/bash
