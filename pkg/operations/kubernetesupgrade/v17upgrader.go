695b1ffd19d3d0c9365389cba842626df0b51684
Dmitry Shmulevich <dmitsh@microsoft.com>
2017-08-09 16:10:27 -0700

    upgrader refactoring

--- pkg/operations/kubernetesupgrade/v17upgrader.go
+++ pkg/operations/kubernetesupgrade/v17upgrader.go
@@ -11,7 +11,7 @@ var _ UpgradeWorkFlow = &Kubernetes17upgrader{}
 
 // Kubernetes17upgrader upgrades a Kubernetes 1.6 cluster to 1.7
 type Kubernetes17upgrader struct {
-	Kubernetes16upgrader
+	Upgrader
 }
 
 // ClusterPreflightCheck does preflight check
@@ -24,15 +24,5 @@ func (ku *Kubernetes17upgrader) ClusterPreflightCheck() error {
 		return fmt.Errorf("Upgrade to Kubernetes 1.7 is not supported from orchestrator release: %s",
 			ku.DataModel.Properties.OrchestratorProfile.OrchestratorRelease)
 	}
-	return nil
-}
-
-// RunUpgrade runs the upgrade pipeline
-func (ku *Kubernetes17upgrader) RunUpgrade() error {
-	return ku.Kubernetes16upgrader.RunUpgrade()
-}
-
-// Validate will run validation post upgrade
-func (ku *Kubernetes17upgrader) Validate() error {
 	return nil
 }
