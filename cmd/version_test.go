ee1fb20bfbc992f3605e7030f658e97caf3a6818
Seth Goings <sgoings@users.noreply.github.com>
2017-08-11 09:54:40 -0500

    improve version command
    - always output git sha (plus dirty or clean)
    - if we're building a tag, show that
    - remove build timestamp

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -13,7 +13,6 @@ var _ = Describe("the version command", func() {
 		command := newVersionCmd()
 		hook := logtest.NewGlobal()
 		command.Run(command, nil)
-		Expect(hook.LastEntry().Message).To(Equal(fmt.Sprintf("ACS-Engine Version: %s", BuildSHA)))
-
+		Expect(hook.LastEntry().Message).To(Equal(fmt.Sprintf("ACS-Engine Version: %s (%s)", BuildTag, BuildSHA)))
 	})
 })
