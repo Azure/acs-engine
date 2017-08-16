3d01dbeb0b84e13d8545d5eb45333a1958c6d0fc
Jack <jaquincy@microsoft.com>
2017-08-15 17:11:07 -0700

    allowing master to have offset
    TODO handle vmss agent pools since they don't have an offset

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -1485,16 +1485,16 @@ func getLinkedTemplatesForExtensions(properties *api.Properties) string {
 
 func getMasterLinkedTemplateText(masterProfile *api.MasterProfile, orchestratorType string, extensionProfile api.ExtensionProfile, singleOrAll string) (string, error) {
 	extTargetVMNamePrefix := "variables('masterVMNamePrefix')"
-	loopCount := masterProfile.Count
+	loopCount := "[sub(variables('masterCount'), variables('masterOffset'))]"
 	if strings.EqualFold(singleOrAll, "single") {
-		loopCount = 1
+		loopCount = "1"
 	}
-	return internalGetPoolLinkedTemplateText(extTargetVMNamePrefix, orchestratorType, strconv.Itoa(loopCount), "", extensionProfile)
+	return internalGetPoolLinkedTemplateText(extTargetVMNamePrefix, orchestratorType, loopCount,
+		"variables('masterOffset')", extensionProfile)
 }
 
 func getAgentPoolLinkedTemplateText(agentPoolProfile *api.AgentPoolProfile, orchestratorType string, extensionProfile api.ExtensionProfile, singleOrAll string) (string, error) {
 	extTargetVMNamePrefix := fmt.Sprintf("variables('%sVMNamePrefix')", agentPoolProfile.Name)
-
 	loopCount := fmt.Sprintf("[sub(variables('%sCount'), variables('%sOffset'))]",
 		agentPoolProfile.Name, agentPoolProfile.Name)
 	if strings.EqualFold(singleOrAll, "single") {
@@ -1502,7 +1502,8 @@ func getAgentPoolLinkedTemplateText(agentPoolProfile *api.AgentPoolProfile, orch
 	}
 	loopOffset := fmt.Sprintf("variables('%sOffset')", agentPoolProfile.Name)
 
-	return internalGetPoolLinkedTemplateText(extTargetVMNamePrefix, orchestratorType, loopCount, loopOffset, extensionProfile)
+	return internalGetPoolLinkedTemplateText(extTargetVMNamePrefix, orchestratorType, loopCount,
+		loopOffset, extensionProfile)
 }
 
 func internalGetPoolLinkedTemplateText(extTargetVMNamePrefix, orchestratorType, loopCount, loopOffset string, extensionProfile api.ExtensionProfile) (string, error) {
