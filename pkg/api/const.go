3cabf82151801db73c9d0bfa5403fa197429a2b2
Jack Francis <jack.francis@microsoft.com>
2017-08-24 10:58:21 -0700

    update api constants

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -82,7 +82,7 @@ var DCOSReleaseToVersion = map[string]string{
 // KubernetesReleaseToVersion maps a major.minor release to an full major.minor.patch version
 var KubernetesReleaseToVersion = map[string]string{
 	KubernetesRelease1Dot7: "1.7.5",
-	KubernetesRelease1Dot6: "1.6.6",
+	KubernetesRelease1Dot6: "1.6.8",
 	KubernetesRelease1Dot5: "1.5.7",
 }
 
