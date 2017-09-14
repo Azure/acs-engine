1245e26611ae2f084f55d3b45d35c55d5c115527
Jack Francis <jack.francis@microsoft.com>
2017-09-14 11:33:37 -0700

    pseudo code for master cert generation

--- parts/kubernetesmastergenerateproxycertscript.sh
+++ parts/kubernetesmastergenerateproxycertscript.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 #TODO write script
+
+# PSEUDO CODE
+# block until all etcd is ready
+# for each cert:
+#  get cert from central datastore on cluster (etcd?)
+#  if success
+#    cert = get response
+#    write cert payload to local disk at appropriate filepath
+#  if fail
+#    generate new cert
+#    put new cert if not exist into central datastore (etcdctl mk /example/key data)
+#    if success
+#      cert = new cert
+#      write cert payload to local disk at appropriate filepath
+#    if fail
+#    retry everything in this enumeration
