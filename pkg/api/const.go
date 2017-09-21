d3ff8ae6f3a3f6e0bf9858448031ff32d6b9b988
Martell Malone <martellmalone@gmail.com>
2017-09-21 21:04:08 +0100

    k8s: update 1.6 to 1.6.10

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -98,7 +98,7 @@ var DCOSReleaseToVersion = map[string]string{
 // KubernetesReleaseToVersion maps a major.minor release to an full major.minor.patch version
 var KubernetesReleaseToVersion = map[string]string{
 	KubernetesRelease1Dot7: "1.7.5",
-	KubernetesRelease1Dot6: "1.6.9",
+	KubernetesRelease1Dot6: "1.6.10",
 	KubernetesRelease1Dot5: "1.5.7",
 }
 
