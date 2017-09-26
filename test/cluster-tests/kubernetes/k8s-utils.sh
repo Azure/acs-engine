862b3d1c4cca237235a2aac796108e607ea3458d
Jack Francis <jack.francis@microsoft.com>
2017-09-26 13:22:09 -0700

    this count value increase is unnecessary
    retry doesn’t actually work currently in the event of no outbound internet access: resolver failure for www.bing.com short-circuits the while loop

--- test/cluster-tests/kubernetes/k8s-utils.sh
+++ test/cluster-tests/kubernetes/k8s-utils.sh
@@ -131,7 +131,7 @@ function test_windows_deployment() {
   # TODO: There is an issue Windows POD has delay to talk to DNS, but not to other services
   sleep 600
 
-  count=30
+  count=10
   success="n"
   while (( $count > 0 )); do
     log "  ... counting down $count"
