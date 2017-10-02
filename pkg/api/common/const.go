fb9031e6739db21571fd6a5236607277cad4d05b
Lachlan Evenson <lachie83@users.noreply.github.com>
2017-10-02 13:09:45 -0700

    Update kubedns and kubernetes versions (#1513)
    * update kubedns and kubernetes versions
    
    * update version constants
    
    * api constants

--- pkg/api/common/const.go
+++ pkg/api/common/const.go
@@ -54,8 +54,8 @@ const (
 // KubeReleaseToVersion maps a major.minor release to an full major.minor.patch version
 var KubeReleaseToVersion = map[string]string{
 	KubernetesRelease1Dot8: "1.8.0",
-	KubernetesRelease1Dot7: "1.7.5",
-	KubernetesRelease1Dot6: "1.6.9",
+	KubernetesRelease1Dot7: "1.7.7",
+	KubernetesRelease1Dot6: "1.6.11",
 	KubernetesRelease1Dot5: "1.5.7",
 }
 
--- pkg/api/const.go
+++ pkg/api/const.go
@@ -100,8 +100,8 @@ var DCOSReleaseToVersion = map[string]string{
 // KubernetesReleaseToVersion maps a major.minor release to an full major.minor.patch version
 var KubernetesReleaseToVersion = map[string]string{
 	KubernetesRelease1Dot8: "1.8.0",
-	KubernetesRelease1Dot7: "1.7.5",
-	KubernetesRelease1Dot6: "1.6.9",
+	KubernetesRelease1Dot7: "1.7.7",
+	KubernetesRelease1Dot6: "1.6.11",
 	KubernetesRelease1Dot5: "1.5.7",
 }
 
