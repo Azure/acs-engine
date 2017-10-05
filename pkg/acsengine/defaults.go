cb6dd960b66cc710467ab61a2aa4906a7829d0eb
CecileRobertMichon <cerobert@microsoft.com>
2017-10-05 11:18:59 -0700

    Revert German version back to 201701130

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -83,7 +83,7 @@ var (
 				ImageOffer:     "UbuntuServer",
 				ImageSku:       "16.04-LTS",
 				ImagePublisher: "Canonical",
-				ImageVersion:   "16.04.201708151",
+				ImageVersion:   "16.04.201701130",
 			},
 			api.RHEL: DefaultRHELOSImageConfig,
 		},
