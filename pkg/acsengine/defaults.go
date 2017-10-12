9b89b7abdced74b0450fe4adee441e3e9fa4c167
Anthony Howe <anhowe@users.noreply.github.com>
2017-10-12 16:06:07 -0700

    add latest ubuntu image (#1589)

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -38,9 +38,9 @@ var (
 	//DefaultUbuntuImageConfig is the default Linux distribution.
 	DefaultUbuntuImageConfig = AzureOSImageConfig{
 		ImageOffer:     "UbuntuServer",
-		ImageSku:       "16.04-LTS",
+		ImageSku:       "16.04-DAILY-LTS",
 		ImagePublisher: "Canonical",
-		ImageVersion:   "16.04.201708151",
+		ImageVersion:   "16.04.201710110",
 	}
 
 	//DefaultRHELOSImageConfig is the RHEL Linux distribution.
