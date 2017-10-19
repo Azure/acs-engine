bfadd5cfdef6eba9d81e1c51381d119de8cc49a3
Jonathan Chauncey <jonathan.chauncey@microsoft.com>
2017-10-19 13:23:43 -0400

    ref(runner.go): Move test runner and provisioning into its own structs

--- test/e2e/config/config.go
+++ test/e2e/config/config.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
@@ -22,8 +21,6 @@ type Config struct {
 	Location          string        `envconfig:"LOCATION" required:"true" default:"southcentralus"`                     // Location where you want to create the cluster
 	ClusterDefinition string        `envconfig:"CLUSTER_DEFINITION" required:"true" default:"examples/kubernetes.json"` // ClusterDefinition is the path on disk to the json template these are normally located in examples/
 	CleanUpOnExit     bool          `envconfig:"CLEANUP_ON_EXIT" default:"true"`                                        // if set the tests will not clean up rgs when tests finish
-	ProvisionRetries  int           `envcofnig:"PROVISION_RETRIES" default:"3"`
-	CreateVNET        bool          `envconfig:"CREATE_VNET" default:"false"`
 	Timeout           time.Duration `envconfig:"TIMEOUT" default:"10m"`
 	CurrentWorkingDir string
 }
@@ -42,14 +39,6 @@ func ParseConfig() (*Config, error) {
 		return nil, err
 	}
 	return c, nil
-}
-
-// GenerateName will generate a new name if one has not been set
-func (c *Config) GenerateName() string {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	suffix := r.Intn(99999)
-	prefix := fmt.Sprintf("%s-%s", c.Orchestrator, c.Location)
-	return fmt.Sprintf("%s-%v", prefix, suffix)
 }
 
 // GetKubeConfig returns the absolute path to the kubeconfig for c.Location
--- test/e2e/runner.go
+++ test/e2e/runner.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"os/signal"
-	"path/filepath"
-	"time"
 
 	"github.com/Azure/acs-engine/test/e2e/azure"
 	"github.com/Azure/acs-engine/test/e2e/config"
-	"github.com/Azure/acs-engine/test/e2e/dcos"
 	"github.com/Azure/acs-engine/test/e2e/engine"
-	"github.com/Azure/acs-engine/test/e2e/kubernetes/node"
 	"github.com/Azure/acs-engine/test/e2e/metrics"
+	"github.com/Azure/acs-engine/test/e2e/runner"
 )
 
 var (
@@ -55,19 +50,16 @@ func main() {
 
 	// Only provision a cluster if there isnt a name present
 	if cfg.Name == "" {
-		for i := 1; i <= cfg.ProvisionRetries; i++ {
-			pt.SetProvisionStart()
-			success := provisionCluster()
-			rgs = append(rgs, cfg.Name)
-			if success {
-				pt.RecordProvisionSuccess()
-				break
-			} else if i == cfg.ProvisionRetries {
-				pt.RecordProvisionError()
-				teardown()
-				log.Fatalf("Exceeded Provision retry count!")
-			}
-			pt.RecordProvisionError()
+		cliProvisioner, err := runner.BuildCLIProvisioner(cfg, acct, pt)
+		if err != nil {
+			log.Fatalf("Error while trying to build CLI Provisioner:%s", err)
+		}
+		err = cliProvisioner.Run()
+		rgs = cliProvisioner.ResourceGroups
+		eng = cliProvisioner.Engine
+		if err != nil {
+			teardown()
+			log.Printf("Error while trying to provision cluster:%s", err)
 		}
 	} else {
 		engCfg, err := engine.ParseConfig(cfg.CurrentWorkingDir, cfg.ClusterDefinition, cfg.Name)
@@ -87,43 +79,14 @@ func main() {
 		}
 	}
 
-	pt.SetNodeWaitStart()
-	if cfg.IsKubernetes() {
-		os.Setenv("KUBECONFIG", cfg.GetKubeConfig())
-		log.Printf("Kubeconfig:%s\n", cfg.GetKubeConfig())
-		log.Println("Waiting on nodes to go into ready state...")
-		ready := node.WaitOnReady(eng.NodeCount(), 10*time.Second, cfg.Timeout)
-		if ready == false {
-			pt.RecordNodeWait()
-			teardown()
-			log.Fatalf("Error: Not all nodes in ready state!")
-		}
-	}
-
-	if cfg.IsDCOS() {
-		host := fmt.Sprintf("%s.%s.cloudapp.azure.com", cfg.Name, cfg.Location)
-		user := eng.ClusterDefinition.Properties.LinuxProfile.AdminUsername
-		log.Printf("SSH Key: %s\n", cfg.GetSSHKeyPath())
-		log.Printf("Master Node: %s@%s\n", user, host)
-		log.Printf("SSH Command: ssh -i %s -p 2200 %s@%s", cfg.GetSSHKeyPath(), user, host)
-		cluster := dcos.NewCluster(cfg, eng)
-		err = cluster.InstallDCOSClient()
+	if !cfg.SkipTest {
+		g, err := runner.ParseGinkgoConfig()
 		if err != nil {
 			teardown()
-			log.Fatalf("Error trying to install dcos client:%s\n", err)
+			log.Fatalf("Error: Unable to parse ginkgo configuration!")
 		}
-		ready := cluster.WaitForNodes(eng.NodeCount(), 10*time.Second, cfg.Timeout)
-		if ready == false {
-			pt.RecordNodeWait()
-			teardown()
-			log.Fatal("Error: Not all nodes in healthy state!")
-		}
-	}
-	pt.RecordNodeWait()
-
-	if !cfg.SkipTest {
 		pt.SetTestStart()
-		err := runGinkgo(cfg.Orchestrator)
+		err = g.Run()
 		if err != nil {
 			pt.RecordTestError()
 			teardown()
@@ -160,99 +123,4 @@ func teardown() {
 			acct.DeleteGroup(rg)
 		}
 	}
-}
-
-func runGinkgo(orchestrator string) error {
-	testDir := fmt.Sprintf("test/e2e/%s", orchestrator)
-	cmd := exec.Command("ginkgo", "-nodes", "10", "-slowSpecThreshold", "180", "-r", testDir)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Start()
-	if err != nil {
-		log.Printf("Error while trying to start ginkgo:%s\n", err)
-		return err
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func provisionCluster() bool {
-	cfg.Name = cfg.GenerateName()
-	os.Setenv("NAME", cfg.Name)
-	log.Printf("Cluster name:%s\n", cfg.Name)
-
-	outputPath := filepath.Join(cfg.CurrentWorkingDir, "_output")
-	os.RemoveAll(outputPath)
-	os.Mkdir(outputPath, 0755)
-
-	out, err := exec.Command("ssh-keygen", "-f", cfg.GetSSHKeyPath(), "-q", "-N", "", "-b", "2048", "-t", "rsa").CombinedOutput()
-	if err != nil {
-		log.Printf("Error while trying to generate ssh key:%s\n\nOutput:%s\n", err, out)
-		return false
-	}
-	exec.Command("chmod", "0600", cfg.GetSSHKeyPath()+"*")
-
-	publicSSHKey, err := cfg.ReadPublicSSHKey()
-	if err != nil {
-		log.Printf("Error while trying to read public ssh key: %s\n", err)
-		return false
-	}
-	os.Setenv("PUBLIC_SSH_KEY", publicSSHKey)
-	os.Setenv("DNS_PREFIX", cfg.Name)
-
-	err = acct.CreateGroup(cfg.Name, cfg.Location)
-	if err != nil {
-		log.Printf("Error while trying to create resource group: %s\n", err)
-		return false
-	}
-
-	subnetID := ""
-	vnetName := fmt.Sprintf("%sCustomVnet", cfg.Name)
-	subnetName := fmt.Sprintf("%sCustomSubnet", cfg.Name)
-	if cfg.CreateVNET {
-		err = acct.CreateVnet(vnetName, "10.239.0.0/16", subnetName, "10.239.0.0/16")
-		if err != nil {
-			return false
-		}
-		subnetID = fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/virtualNetworks/%s/subnets/%s", acct.SubscriptionID, acct.ResourceGroup.Name, vnetName, subnetName)
-	}
-
-	// Lets modify our template and call acs-engine generate on it
-	eng, err = engine.Build(cfg, subnetID)
-	if err != nil {
-		log.Printf("Error while trying to build cluster definition: %s\n", err)
-		return false
-	}
-
-	err = eng.Write()
-	if err != nil {
-		log.Printf("Error while trying to write Engine Template to disk:%s\n", err)
-		return false
-	}
-
-	err = eng.Generate()
-	if err != nil {
-		log.Printf("Error while trying to generate acs-engine template: %s\n", err)
-		return false
-	}
-
-	// Lets start by just using the normal az group deployment cli for creating a cluster
-	log.Println("Creating deployment this make take a few minutes...")
-	err = acct.CreateDeployment(cfg.Name, eng)
-	if err != nil {
-		return false
-	}
-
-	if cfg.CreateVNET {
-		err = acct.UpdateRouteTables(subnetName, vnetName)
-		if err != nil {
-			return false
-		}
-	}
-
-	return true
 }
