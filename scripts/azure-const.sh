d67667fd0740b49495a1f3ce5ec0f8759aff1417
Jonathan Chauncey <jonathan.chauncey@microsoft.com>
2017-10-31 17:46:57 -0400

    update azure-const.sh to do an azure login

--- scripts/azure-const.sh
+++ scripts/azure-const.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+if [ -z "$CLIENT_ID" ]; then
+    echo "must provide a CLIENT_ID env var"
+    exit 1;
+fi
+
+if [ -z "$CLIENT_SECRET" ]; then
+    echo "must provide a CLIENT_SECRET env var"
+    exit 1;
+fi
+
+if [ -z "$TENANT_ID" ]; then
+    echo "must provide a TENANT_ID env var"
+    exit 1;
+fi
+
+if [ -z "$SUBSCRIPTION_ID_TO_CLEANUP" ]; then
+    echo "must provide a SUBSCRIPTION_ID_TO_CLEANUP env var"
+    exit 1;
+fi
+
+set -eu -o pipefail
+
+az login --service-principal \
+		--username "${CLIENT_ID}" \
+		--password "${CLIENT_SECRET}" \
+		--tenant "${TENANT_ID}" &>/dev/null
+
+# set to the sub id we want to cleanup
+az account set -s $SUBSCRIPTION_ID
 
 python pkg/acsengine/Get-AzureConstants.py
 git status | grep pkg/acsengine/azureconst.go
