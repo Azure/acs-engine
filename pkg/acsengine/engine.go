7fc46be53997785706632be602a6c9c263f1b04c
Jack Francis <jackfrancis@gmail.com>
2017-11-04 12:20:01 -0700

    Revert "Added cluster name to node labels (#1653)"
    This reverts commit 13cf61f625c1850c07dc15947a9988a8c1783504.

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -756,16 +756,9 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.ContainerService) templat
 			constraint, _ := semver.NewConstraint("~" + version)
 			return cs.Properties.OrchestratorProfile.OrchestratorType == api.Kubernetes && constraint.Check(orchestratorVersion)
 		},
-		"GetMasterKubernetesLabels": func(rg string) string {
+		"GetKubernetesLabels": func(profile *api.AgentPoolProfile) string {
 			var buf bytes.Buffer
-			buf.WriteString(fmt.Sprintf("kubernetes.azure.com/cluster=%s", rg))
-			buf.WriteString(",kubernetes.io/role=master")
-			return buf.String()
-		},
-		"GetAgentKubernetesLabels": func(profile *api.AgentPoolProfile, rg string) string {
-			var buf bytes.Buffer
-			buf.WriteString(fmt.Sprintf("kubernetes.azure.com/cluster=%s", rg))
-			buf.WriteString(fmt.Sprintf(",kubernetes.io/role=agent,agentpool=%s", profile.Name))
+			buf.WriteString(fmt.Sprintf("kubernetes.io/role=agent,agentpool=%s", profile.Name))
 			if profile.StorageProfile == api.ManagedDisks {
 				storagetier, _ := getStorageAccountType(profile.VMSize)
 				buf.WriteString(fmt.Sprintf(",storageprofile=managed,storagetier=%s", storagetier))
