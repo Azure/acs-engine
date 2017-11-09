1429e7ed51dafff02f2b2494bd967fb23f0ebbc0
Cecile Robert-Michon <cerobert@microsoft.com>
2017-11-09 11:30:21 -0800

    K8s versions 1.6.12 & 1.7.10 (#1722)
    * merge master upstream
    
    * Revert "merge master upstream"
    
    This reverts commit ab8d33d28a2b5e9a419e0018c9fee82b0134b11e.
    
    * Kubenetes 1.6.12 and 1.7.10
    
    * fix typo
    
    * fix typo
    
    * more fixes
    
    * fix upgradecluster_test
    
    * added windows supported version 1.7.10
    
    * updated win zip
    
    * fix win zip 1.7.10
    
    * remove space
    
    * solve conflict
    
    * removed win zip for 1.6.12

--- pkg/api/common/const.go
+++ pkg/api/common/const.go
@@ -73,18 +73,22 @@ const (
 	KubernetesVersion1Dot7Dot7 string = "1.7.7"
 	// KubernetesVersion1Dot7Dot9 is the major.minor.patch string for the 1.7.9 version of kubernetes
 	KubernetesVersion1Dot7Dot9 string = "1.7.9"
+	// KubernetesVersion1Dot7Dot10 is the major.minor.patch string for the 1.7.10 version of kubernetes
+	KubernetesVersion1Dot7Dot10 string = "1.7.10"
 	// KubernetesVersion1Dot6Dot6 is the major.minor.patch string for the 1.6.6 version of kubernetes
 	KubernetesVersion1Dot6Dot6 string = "1.6.6"
 	// KubernetesVersion1Dot6Dot9 is the major.minor.patch string for the 1.6.9 version of kubernetes
 	KubernetesVersion1Dot6Dot9 string = "1.6.9"
 	// KubernetesVersion1Dot6Dot11 is the major.minor.patch string for the 1.6.11 version of kubernetes
 	KubernetesVersion1Dot6Dot11 string = "1.6.11"
+	// KubernetesVersion1Dot6Dot12 is the major.minor.patch string for the 1.6.12 version of kubernetes
+	KubernetesVersion1Dot6Dot12 string = "1.6.12"
 	// KubernetesVersion1Dot5Dot7 is the major.minor.patch string for the 1.5.7 version of kubernetes
 	KubernetesVersion1Dot5Dot7 string = "1.5.7"
 	// KubernetesVersion1Dot5Dot8 is the major.minor.patch string for the 1.5.8 version of kubernetes
 	KubernetesVersion1Dot5Dot8 string = "1.5.8"
 	// KubernetesDefaultVersion is the default major.minor.patch version for kubernetes
-	KubernetesDefaultVersion string = KubernetesVersion1Dot7Dot9
+	KubernetesDefaultVersion string = KubernetesVersion1Dot7Dot10
 )
 
 // AllKubernetesSupportedVersions is a whitelist map of supported Kubernetes version strings
@@ -94,6 +98,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	KubernetesVersion1Dot6Dot6:  true,
 	KubernetesVersion1Dot6Dot9:  true,
 	KubernetesVersion1Dot6Dot11: true,
+	KubernetesVersion1Dot6Dot12: true,
 	KubernetesVersion1Dot7Dot0:  true,
 	KubernetesVersion1Dot7Dot1:  true,
 	KubernetesVersion1Dot7Dot2:  true,
@@ -101,6 +106,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	KubernetesVersion1Dot7Dot5:  true,
 	KubernetesVersion1Dot7Dot7:  true,
 	KubernetesVersion1Dot7Dot9:  true,
+	KubernetesVersion1Dot7Dot10: true,
 	KubernetesVersion1Dot8Dot0:  true,
 	KubernetesVersion1Dot8Dot1:  true,
 	KubernetesVersion1Dot8Dot2:  true,
@@ -125,14 +131,15 @@ func GetAllSupportedKubernetesVersions() []string {
 
 // AllKubernetesWindowsSupportedVersions maintain a set of available k8s Windows versions in acs-engine
 var AllKubernetesWindowsSupportedVersions = map[string]bool{
-	KubernetesVersion1Dot7Dot2: true,
-	KubernetesVersion1Dot7Dot4: true,
-	KubernetesVersion1Dot7Dot5: true,
-	KubernetesVersion1Dot7Dot7: true,
-	KubernetesVersion1Dot7Dot9: true,
-	KubernetesVersion1Dot8Dot0: true,
-	KubernetesVersion1Dot8Dot1: true,
-	KubernetesVersion1Dot8Dot2: true,
+	KubernetesVersion1Dot7Dot2:  true,
+	KubernetesVersion1Dot7Dot4:  true,
+	KubernetesVersion1Dot7Dot5:  true,
+	KubernetesVersion1Dot7Dot7:  true,
+	KubernetesVersion1Dot7Dot9:  true,
+	KubernetesVersion1Dot7Dot10: true,
+	KubernetesVersion1Dot8Dot0:  true,
+	KubernetesVersion1Dot8Dot1:  true,
+	KubernetesVersion1Dot8Dot2:  true,
 }
 
 const (
