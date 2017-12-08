8979c827634984957eb3d0361f83669aca03d590
CecileRobertMichon <cerobert@microsoft.com>
2017-12-08 14:44:21 -0800

    rename script to reflect changes

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -31,7 +31,7 @@ const (
 	kubernetesMasterCustomScript             = "k8s/kubernetesmastercustomscript.sh"
 	kubernetesMountetcd                      = "k8s/kubernetes_mountetcd.sh"
 	kubernetesMasterGenerateProxyCertsScript = "k8s/kubernetesmastergenerateproxycertscript.sh"
-	kubernetesMasterGenerateEtcdCertsScript  = "k8s/kubernetesmastergenerateetcdcertscript.sh"
+	kubernetesMasterEtcdTLSScript            = "k8s/kubernetesmasterreconfigureetcdtls.sh"
 	kubernetesAgentCustomDataYaml            = "k8s/kubernetesagentcustomdata.yml"
 	kubeConfigJSON                           = "k8s/kubeconfig.json"
 	kubernetesWindowsAgentCustomDataPS1      = "k8s/kuberneteswindowssetup.ps1"
@@ -112,14 +112,14 @@ var kubernetesManifestYamls = map[string]string{
 var kubernetesAritfacts = map[string]string{
 	"MASTER_PROVISION_B64_GZIP_STR":            kubernetesMasterCustomScript,
 	"MASTER_GENERATE_PROXY_CERTS_B64_GZIP_STR": kubernetesMasterGenerateProxyCertsScript,
-	"MASTER_GENERATE_ETCD_CERTS_B64_GZIP_STR":  kubernetesMasterGenerateEtcdCertsScript,
+	"MASTER_GENERATE_ETCD_CERTS_B64_GZIP_STR":  kubernetesMasterEtcdTLSScript,
 	"KUBELET_SERVICE_B64_GZIP_STR":             kubernetesKubeletService,
 }
 
 var kubernetesAritfacts15 = map[string]string{
 	"MASTER_PROVISION_B64_GZIP_STR":            kubernetesMasterCustomScript,
 	"MASTER_GENERATE_PROXY_CERTS_B64_GZIP_STR": kubernetesMasterGenerateProxyCertsScript,
-	"MASTER_GENERATE_ETCD_CERTS_B64_GZIP_STR":  kubernetesMasterGenerateEtcdCertsScript,
+	"MASTER_GENERATE_ETCD_CERTS_B64_GZIP_STR":  kubernetesMasterEtcdTLSScript,
 	"KUBELET_SERVICE_B64_GZIP_STR":             "k8s/kuberneteskubelet1.5.service",
 }
 
@@ -1096,8 +1096,8 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.ContainerService) templat
 		"GetKubernetesB64GenerateProxyCerts": func() string {
 			return getBase64CustomScript(kubernetesMasterGenerateProxyCertsScript)
 		},
-		"GetKubernetesB64GenerateEtcdCerts": func() string {
-			return getBase64CustomScript(kubernetesMasterGenerateEtcdCertsScript)
+		"GetKubernetesB64EtcdTLS": func() string {
+			return getBase64CustomScript(kubernetesMasterEtcdTLSScript)
 		},
 		"GetKubernetesMasterPreprovisionYaml": func() string {
 			str := ""
