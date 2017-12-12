8243c497a00a501ce24b2ebacb652b17c2e74aa7
Jack Francis <jack.francis@microsoft.com>
2017-12-12 11:56:54 -0800

    compare as strings

--- test/e2e/cleanup.sh
+++ test/e2e/cleanup.sh
@@ -42,7 +42,7 @@ az account set -s $SUBSCRIPTION_ID_TO_CLEANUP
 (( deadline=$(date +%s)-${expirationInSecs%.*} ))
 # find resource groups created before our deadline
 echo "Looking for resource groups created over ${EXPIRATION_IN_HOURS} hours ago..."
-for resourceGroup in `az group list | jq --arg dl "${deadline}" '.[] | select(.id | contains("acse-test-infrastructure") | not) | select(.tags.now|tonumber < $dl).name' | tr -d '\"'`; do
+for resourceGroup in `az group list | jq --arg dl $deadline '.[] | select(.id | contains("acse-test-infrastructure") | not) | select(.tags.now < $dl).name' | tr -d '\"'`; do
     echo "Will delete resource group ${resourceGroup}..."
     # delete old resource groups
     az group delete -y -n $resourceGroup --no-wait >> delete.log
