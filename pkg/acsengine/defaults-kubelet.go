2fa578c25da2e5e267874850ee47676f207256f6
Jack Francis <jackfrancis@gmail.com>
2018-01-18 13:10:51 -0800

    re-enable read-only port on kubelet (#2091)
    fixes heapster connection issues

--- pkg/acsengine/defaults-kubelet.go
+++ pkg/acsengine/defaults-kubelet.go
@@ -27,7 +27,6 @@ func setKubeletConfig(cs *api.ContainerService) {
 		"--enforce-node-allocatable":        "",
 		"--kubeconfig":                      "/var/lib/kubelet/kubeconfig",
 		"--azure-container-registry-config": "/etc/kubernetes/azure.json",
-		"--read-only-port":                  "0",
 		"--keep-terminated-pod-volumes":     "false",
 	}
 
