b1b6ddedb54f74897e7928748f37741c576f02b8
Cecile Robert-Michon <cerobert@microsoft.com>
2018-01-17 16:45:32 -0800

    Jenkins soak tests (#2028)
    * added soak test
    
    * added soak test name
    
    * add location to name
    
    * delete rg if re-provisioning cluster
    
    * fix typo
    
    * added wait flag
    
    * fix no wait arg
    
    * add err log
    
    * remove ssh key
    
    * remove unused vars
    
    * Revert "remove ssh key"
    
    This reverts commit 9041c1a3eee37f3d699827f14fbf0dd25df556f4.
    
    * terminology
    
    * delete files if deployment failed
    
    * remove soak test spec
    
    * Pass in soak cluster name
    
    * only generate ssh key once
    
    * add default for KeyGenerated
    
    * fix typo
    
    * Do not generate SSH for soak test
    
    * fix typo in comment
    
    * add dashboard test debug log
    
    * Revert "add dashboard test debug log"
    
    This reverts commit 49372822a79469cdbb45cab7d7887e70c6ba2c19.

--- test/e2e/azure/cli.go
+++ test/e2e/azure/cli.go
@@ -95,9 +95,15 @@ func (a *Account) CreateGroup(name, location string) error {
 	return nil
 }
 
-// DeleteGroup delets a given resource group by name
-func (a *Account) DeleteGroup(name string) error {
-	out, err := exec.Command("az", "group", "delete", "--name", name, "--no-wait", "--yes").CombinedOutput()
+// DeleteGroup deletes a given resource group by name
+func (a *Account) DeleteGroup(name string, wait bool) error {
+	var out []byte
+	var err error
+	if !wait {
+		out, err = exec.Command("az", "group", "delete", "--name", name, "--no-wait", "--yes").CombinedOutput()
+	} else {
+		out, err = exec.Command("az", "group", "delete", "--name", name, "--yes").CombinedOutput()
+	}
 	if err != nil {
 		log.Printf("Error while trying to delete resource group (%s):%s", name, out)
 		return err
