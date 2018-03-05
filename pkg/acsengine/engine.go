74f2ef1ba985bb876c4a6c85f590ed24e2f994f1
Dong Liu <doliu@microsoft.com>
2018-03-06 06:04:11 +0800

    Remove deprecated '--require-kubeconfig' for k8s (#2365)
    * Remove deprecated --require-kubeconfig
    
    * adding —require-kubeconfig back to 1.7j
    
    * less than is what we want here

--- pkg/acsengine/engine.go
+++ pkg/acsengine/engine.go
@@ -846,6 +846,11 @@ func (t *TemplateGenerator) getTemplateFuncMap(cs *api.ContainerService) templat
 			constraint, _ := semver.NewConstraint(">=" + version)
 			return cs.Properties.OrchestratorProfile.OrchestratorType == api.Kubernetes && constraint.Check(orchestratorVersion)
 		},
+		"IsKubernetesVersionLt": func(version string) bool {
+			orchestratorVersion, _ := semver.NewVersion(cs.Properties.OrchestratorProfile.OrchestratorVersion)
+			constraint, _ := semver.NewConstraint("<" + version)
+			return cs.Properties.OrchestratorProfile.OrchestratorType == api.Kubernetes && constraint.Check(orchestratorVersion)
+		},
 		"IsKubernetesVersionTilde": func(version string) bool {
 			// examples include
 			// ~2.3 is equivalent to >= 2.3, < 2.4
