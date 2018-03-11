6e531f8fe006fd9e5b526b91b1b3f797aac660c0
Jess Frazelle <acidburn@microsoft.com>
2018-03-11 15:01:39 -0400

    network policy: add cilium
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- pkg/api/vlabs/validate.go
+++ pkg/api/vlabs/validate.go
@@ -705,7 +705,7 @@ func (a *Properties) validateNetworkPolicy() error {
 	}
 
 	// Temporary safety check, to be removed when Windows support is added.
-	if (networkPolicy == "calico") && a.HasWindows() {
+	if (networkPolicy == "calico" || networkPolicy == "cilium") && a.HasWindows() {
 		return fmt.Errorf("networkPolicy '%s' is not supporting windows agents", networkPolicy)
 	}
 
