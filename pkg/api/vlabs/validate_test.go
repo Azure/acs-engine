36efa73f7103869f944c7c3fd61804e83cfa307a
Jack Francis <jack.francis@microsoft.com>
2018-03-13 14:49:08 -0700

    more tests

--- pkg/api/vlabs/validate_test.go
+++ pkg/api/vlabs/validate_test.go
@@ -702,7 +702,7 @@ func TestLinuxVersions(t *testing.T) {
 	}
 
 	p = getK8sDefaultProperties(false)
-	p.OrchestratorProfile.OrchestratorVersion = "1.9.3"
+	p.OrchestratorProfile.OrchestratorVersion = "1.9.4"
 	if err := p.Validate(false); err != nil {
 		t.Errorf(
 			"should not error on valid Linux version: %v", err,
@@ -710,7 +710,7 @@ func TestLinuxVersions(t *testing.T) {
 	}
 
 	p = getK8sDefaultProperties(false)
-	p.OrchestratorProfile.OrchestratorVersion = "1.9.4"
+	p.OrchestratorProfile.OrchestratorVersion = "1.9.5"
 	if err := p.Validate(false); err == nil {
 		t.Errorf(
 			"should error on invalid Linux version",
