fcc3650a98f18790f643b91e3a6cb858e7ae39e3
Jack Francis <jack.francis@microsoft.com>
2018-03-23 10:26:17 -0700

    removing unnecessary shifts in retrycmd_get_tarball

--- parts/k8s/kubernetesprovisionsource.sh
+++ parts/k8s/kubernetesprovisionsource.sh
@@ -2,4 +2,4 @@
 
 retrycmd_if_failure() { retries=$1; wait=$2; timeout=$3; shift && shift && shift; for i in $(seq 1 $retries); do timeout $timeout ${@}; [ $? -eq 0  ] && break || sleep $wait; done; echo Executed \"$@\" $i times; }
 retrycmd_if_failure_no_stats() { retries=$1; wait=$2; timeout=$3; shift && shift && shift; for i in $(seq 1 $retries); do timeout $timeout ${@}; [ $? -eq 0  ] && break || sleep $wait; done; }
-retrycmd_get_tarball() { retries=$1; wait=$2; tarball=$3; url=$4 shift && shift && shift && shift; for i in $(seq 1 $retries); do tar -tzf $tarball; [ $? -eq 0  ] && break || retrycmd_if_failure_no_stats $retries 1 10 curl -fsSL $url > $tarball; sleep $wait; done; }
+retrycmd_get_tarball() { retries=$1; wait=$2; tarball=$3; url=$4; for i in $(seq 1 $retries); do tar -tzf $tarball; [ $? -eq 0  ] && break || retrycmd_if_failure_no_stats $retries 1 10 curl -fsSL $url > $tarball; sleep $wait; done; }
