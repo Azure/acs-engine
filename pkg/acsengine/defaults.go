f6be1e66a11eb8ce9b09d24b801d63d9c9197d2e
Bruce Campbell <v-brucc@microsoft.com>
2018-03-29 13:11:18 -0700

    Fixed issue with vanilla 1.11 deploy

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -46,7 +46,8 @@ var (
 		DCOS110BootstrapDownloadURL:     fmt.Sprintf(AzureEdgeDCOSBootstrapDownloadURL, "stable/1.10.0", "4d92536e7381176206e71ee15b5ffe454439920c"),
 		DCOS111BootstrapDownloadURL:     fmt.Sprintf(AzureEdgeDCOSBootstrapDownloadURL, "stable/1.11.0", "a0654657903fb68dff60f6e522a7f241c1bfbf0f"),
 		DCOSWindowsBootstrapDownloadURL: "http://dcos-win.westus.cloudapp.azure.com/dcos-windows/stable/",
-		DcosRepositoryURL:               "https://dcosio.azureedge.net/dcos/stable",
+		DcosRepositoryURL:               "https://dcosio.azureedge.net/dcos/stable/1.11.0",
+		DcosClusterPackageListID:        "248a66388bba1adbcb14a52fd3b7b424ab06fa76",
 	}
 
 	//DefaultDockerSpecConfig is the default Docker engine repo.
