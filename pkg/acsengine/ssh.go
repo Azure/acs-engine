990e951463c9bde45620987b4c16ae205655dcd8
Jack Francis <jackfrancis@gmail.com>
2018-04-10 13:51:33 -0700

    Revert "Fix nil linuxprofile (#2621)"
    This reverts commit 7272611344e09bf7e59d565bb39457d9736511d5.

--- pkg/acsengine/ssh.go
+++ pkg/acsengine/ssh.go
@@ -4,15 +4,26 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
+	"io"
 
-	"github.com/Azure/acs-engine/pkg/helpers"
 	"github.com/Azure/acs-engine/pkg/i18n"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+)
+
+// SSHCreator represents the object that creates SSH key pair
+type SSHCreator struct {
+	Translator *i18n.Translator
+}
+
+const (
+	// SSHKeySize is the size (in bytes) of SSH key to create
+	SSHKeySize = 4096
 )
 
 // CreateSaveSSH generates and stashes an SSH key pair.
-func CreateSaveSSH(username, outputDirectory string, s *i18n.Translator) (privateKey *rsa.PrivateKey, publicKeyString string, err error) {
-
-	privateKey, publicKeyString, err = helpers.CreateSSH(rand.Reader, s)
+func (s *SSHCreator) CreateSaveSSH(username, outputDirectory string) (privateKey *rsa.PrivateKey, publicKeyString string, err error) {
+	privateKey, publicKeyString, err = s.CreateSSH(rand.Reader)
 	if err != nil {
 		return nil, "", err
 	}
@@ -20,7 +31,7 @@ func CreateSaveSSH(username, outputDirectory string, s *i18n.Translator) (privat
 	privateKeyPem := privateKeyToPem(privateKey)
 
 	f := &FileSaver{
-		Translator: s,
+		Translator: s.Translator,
 	}
 
 	err = f.SaveFile(outputDirectory, fmt.Sprintf("%s_rsa", username), privateKeyPem)
@@ -29,4 +40,23 @@ func CreateSaveSSH(username, outputDirectory string, s *i18n.Translator) (privat
 	}
 
 	return privateKey, publicKeyString, nil
+}
+
+// CreateSSH creates an SSH key pair.
+func (s *SSHCreator) CreateSSH(rg io.Reader) (privateKey *rsa.PrivateKey, publicKeyString string, err error) {
+	log.Debugf("ssh: generating %dbit rsa key", SSHKeySize)
+	privateKey, err = rsa.GenerateKey(rg, SSHKeySize)
+	if err != nil {
+		return nil, "", s.Translator.Errorf("failed to generate private key for ssh: %q", err)
+	}
+
+	publicKey := privateKey.PublicKey
+	sshPublicKey, err := ssh.NewPublicKey(&publicKey)
+	if err != nil {
+		return nil, "", s.Translator.Errorf("failed to create openssh public key string: %q", err)
+	}
+	authorizedKeyBytes := ssh.MarshalAuthorizedKey(sshPublicKey)
+	authorizedKey := string(authorizedKeyBytes)
+
+	return privateKey, authorizedKey, nil
 }
