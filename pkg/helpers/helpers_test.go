9ab9fc00634fc5535735e50076adda31617f512d
Elizabeth Halper <elhalper@microsoft.com>
2018-04-26 11:15:42 -0700

    Added unit test for normalize azure region

--- pkg/helpers/helpers_test.go
+++ pkg/helpers/helpers_test.go
@@ -9,3 +9,32 @@ func TestPointerToBool(t *testing.T) {
 		t.Fatalf("expected PointerToBool(true) to return *true, instead returned %#v", ret)
 	}
 }
+
+func TestIsRegionNormalized(t *testing.T) {
+	cases := []struct {
+		input string
+		expectedResult string
+	}{
+		input: "westus",
+		expectedResult: "westus"
+	},
+	{
+		input: "West US",
+		expectedResult: "westus"
+	},
+	{
+		input: "Eastern Africa"
+		expectedResult: "easternafrica"
+	},
+	{
+		input: ""
+		expectedResult: ""
+	}
+
+	for _, c := range cases {
+		result := NormalizeAzureRegion(c.input)
+		if c.expectedResult != result {
+			t.Fatalf("NormalizeAzureRegion returned unexpected result: expected %t but got %t", c.expectedResult, result)
+		}
+	}
+}
