db2cea2134a79e4803af756fdd7ecc3cf5036dd5
Julien Corioland <julien.corioland@live.fr>
2018-04-27 12:58:33 -0700

    refactor api model merger into transform package

--- cmd/generate.go
+++ cmd/generate.go
@@ -6,15 +6,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
-	"regexp"
-	"strconv"
-	"strings"
 
 	"github.com/Azure/acs-engine/pkg/acsengine"
 	"github.com/Azure/acs-engine/pkg/acsengine/transform"
 	"github.com/Azure/acs-engine/pkg/api"
 	"github.com/Azure/acs-engine/pkg/i18n"
-	"github.com/Jeffail/gabs"
 	"github.com/leonelquinteros/gotext"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -40,15 +36,6 @@ type generateCmd struct {
 	containerService *api.ContainerService
 	apiVersion       string
 	locale           *gotext.Locale
-}
-
-type setFlagValue struct {
-	stringValue   string
-	intValue      int64
-	arrayValue    bool
-	arrayIndex    int
-	arrayProperty string
-	arrayName     string
 }
 
 func newGenerateCmd() *cobra.Command {
@@ -120,11 +107,11 @@ func (gc *generateCmd) mergeAPIModel() error {
 
 	// if --set flag has been used
 	if gc.set != nil && len(gc.set) > 0 {
-		m := make(map[string]setFlagValue)
-		mapValues(m, gc.set)
+		m := make(map[string]transform.ApiModelValue)
+		transform.MapValues(m, gc.set)
 
 		// overrides the api model and generates a new file
-		gc.apimodelPath, err = mergeValuesWithAPIModel(gc.apimodelPath, m)
+		gc.apimodelPath, err = transform.MergeValuesWithAPIModel(gc.apimodelPath, m)
 		if err != nil {
 			return fmt.Errorf(fmt.Sprintf("error merging --set values with the api model: %s", err.Error()))
 		}
@@ -220,98 +207,4 @@ func (gc *generateCmd) run() error {
 	}
 
 	return nil
-}
-
-func mapValues(m map[string]setFlagValue, values []string) {
-	if values == nil || len(values) == 0 {
-		return
-	}
-
-	for _, value := range values {
-		splittedValues := strings.Split(value, ",")
-		if len(splittedValues) > 1 {
-			mapValues(m, splittedValues)
-		} else {
-			keyValueSplitted := strings.Split(value, "=")
-			key := keyValueSplitted[0]
-			stringValue := keyValueSplitted[1]
-
-			flagValue := setFlagValue{}
-
-			if asInteger, err := strconv.ParseInt(stringValue, 10, 64); err == nil {
-				flagValue.intValue = asInteger
-			} else {
-				flagValue.stringValue = stringValue
-			}
-
-			// use regex to find array[index].property pattern in the key
-			re := regexp.MustCompile(`(.*?)\[(.*?)\]\.(.*?)$`)
-			match := re.FindStringSubmatch(key)
-
-			// it's an array
-			if len(match) != 0 {
-				i, err := strconv.ParseInt(match[2], 10, 32)
-				if err != nil {
-					log.Warnln(fmt.Sprintf("array index is not specified for property %s", key))
-				} else {
-					arrayIndex := int(i)
-					flagValue.arrayValue = true
-					flagValue.arrayName = match[1]
-					flagValue.arrayIndex = arrayIndex
-					flagValue.arrayProperty = match[3]
-					m[key] = flagValue
-				}
-			} else {
-				m[key] = flagValue
-			}
-		}
-	}
-}
-
-func mergeValuesWithAPIModel(apiModelPath string, m map[string]setFlagValue) (string, error) {
-	// load the apiModel file from path
-	fileContent, err := ioutil.ReadFile(apiModelPath)
-	if err != nil {
-		return "", err
-	}
-
-	// parse the json from file content
-	jsonObj, err := gabs.ParseJSON(fileContent)
-	if err != nil {
-		return "", err
-	}
-
-	// update api model definition with each value in the map
-	for key, flagValue := range m {
-		// working on an array
-		if flagValue.arrayValue {
-			log.Infoln(fmt.Sprintf("--set flag array value detected. Name: %s, Index: %b, PropertyName: %s", flagValue.arrayName, flagValue.arrayIndex, flagValue.arrayProperty))
-			arrayValue := jsonObj.Path(fmt.Sprint("properties.", flagValue.arrayName))
-			if flagValue.stringValue != "" {
-				arrayValue.Index(flagValue.arrayIndex).SetP(flagValue.stringValue, flagValue.arrayProperty)
-			} else {
-				arrayValue.Index(flagValue.arrayIndex).SetP(flagValue.intValue, flagValue.arrayProperty)
-			}
-		} else {
-			if flagValue.stringValue != "" {
-				jsonObj.SetP(flagValue.stringValue, fmt.Sprint("properties.", key))
-			} else {
-				jsonObj.SetP(flagValue.intValue, fmt.Sprint("properties.", key))
-			}
-		}
-	}
-
-	// generate a new file
-	tmpFile, err := ioutil.TempFile("", "mergedApiModel")
-	if err != nil {
-		return "", err
-	}
-
-	tmpFileName := tmpFile.Name()
-	err = ioutil.WriteFile(tmpFileName, []byte(jsonObj.String()), os.ModeAppend)
-	if err != nil {
-		return "", err
-	}
-
-	return tmpFileName, nil
 }
