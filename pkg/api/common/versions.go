d1fd19479f48569ae5e70591c24bd5165e566d7f
CecileRobertMichon <cerobert@microsoft.com>
2018-05-01 15:02:59 -0700

    Remover support for windows

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -201,7 +201,8 @@ func getAllKubernetesWindowsSupportedVersionsMap() map[string]bool {
 		"1.7.1",
 		"1.10.0-beta.2",
 		"1.10.0-beta.4",
-		"1.10.0-rc.1"} {
+		"1.10.0-rc.1",
+		"1.11.0-alpha.1"} {
 		ret[version] = false
 	}
 	return ret
