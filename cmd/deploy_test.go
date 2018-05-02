e53d3d4fa20dbc948c25f1a5860c5fb10701b097
manisbindra <maninder.bindra@gmail.com>
2018-05-02 19:55:50 +0530

    incorporated review comments

--- cmd/deploy_test.go
+++ cmd/deploy_test.go
@@ -148,7 +148,53 @@ func TestAPIModelWithoutServicePrincipalProfileAndClientIdAndSecretInCmd(t *test
 
 	defer os.RemoveAll(deployCmd.outputDirectory)
 
-	if deployCmd.containerService.Properties.ServicePrincipalProfile == nil {
+	if deployCmd.containerService.Properties.ServicePrincipalProfile == nil || deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID == "" || deployCmd.containerService.Properties.ServicePrincipalProfile.Secret == "" {
+		t.Fatalf("expected service principal profile to be populated from deployment command arguments")
+	}
+
+	if deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID != TestClientIDInCmd.String() {
+		t.Fatalf("expected service principal profile client id to be %s but got %s", TestClientIDInCmd.String(), deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID)
+	}
+
+	if deployCmd.containerService.Properties.ServicePrincipalProfile.Secret != TestClientSecretInCmd {
+		t.Fatalf("expected service principal profile client secret to be %s but got %s", TestClientSecretInCmd, deployCmd.containerService.Properties.ServicePrincipalProfile.Secret)
+	}
+}
+
+func TestAPIModelWithEmptyServicePrincipalProfileAndClientIdAndSecretInCmd(t *testing.T) {
+	apiloader := &api.Apiloader{
+		Translator: nil,
+	}
+
+	apimodel := getAPIModel(ExampleAPIModelWithDNSPrefix, false, "", "")
+	TestClientIDInCmd, err := uuid.FromString("DEC923E3-1EF1-4745-9516-37906D56DEC4")
+	if err != nil {
+		t.Fatalf("Invalid ClientID in Test: %s", err)
+	}
+
+	TestClientSecretInCmd := "DEC923E3-1EF1-4745-9516-37906D56DEC4"
+
+	cs, ver, err := apiloader.DeserializeContainerService([]byte(apimodel), false, false, nil)
+	if err != nil {
+		t.Fatalf("unexpected error deserializing the example apimodel: %s", err)
+	}
+	deployCmd := &deployCmd{
+		apimodelPath:     "./this/is/unused.json",
+		outputDirectory:  "_test_output",
+		forceOverwrite:   true,
+		location:         "westus",
+		containerService: cs,
+		apiVersion:       ver,
+
+		client: &armhelpers.MockACSEngineClient{},
+	}
+	deployCmd.ClientID = TestClientIDInCmd
+	deployCmd.ClientSecret = TestClientSecretInCmd
+	autofillApimodel(deployCmd)
+
+	defer os.RemoveAll(deployCmd.outputDirectory)
+
+	if deployCmd.containerService.Properties.ServicePrincipalProfile == nil || deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID == "" || deployCmd.containerService.Properties.ServicePrincipalProfile.Secret == "" {
 		t.Fatalf("expected service principal profile to be populated from deployment command arguments")
 	}
 
@@ -192,6 +238,47 @@ func TestAPIModelWithoutServicePrincipalProfileAndWithoutClientIdAndSecretInCmd(
 
 }
 
+func TestAPIModelWithEmptyServicePrincipalProfileAndWithoutClientIdAndSecretInCmd(t *testing.T) {
+	apiloader := &api.Apiloader{
+		Translator: nil,
+	}
+
+	apimodel := getAPIModel(ExampleAPIModelWithDNSPrefix, false, "", "")
+
+	cs, ver, err := apiloader.DeserializeContainerService([]byte(apimodel), false, false, nil)
+	if err != nil {
+		t.Fatalf("unexpected error deserializing the example apimodel: %s", err)
+	}
+	deployCmd := &deployCmd{
+		apimodelPath:     "./this/is/unused.json",
+		outputDirectory:  "_test_output",
+		forceOverwrite:   true,
+		location:         "westus",
+		containerService: cs,
+		apiVersion:       ver,
+
+		client: &armhelpers.MockACSEngineClient{},
+	}
+	autofillApimodel(deployCmd)
+
+	defer os.RemoveAll(deployCmd.outputDirectory)
+
+	if deployCmd.containerService.Properties.ServicePrincipalProfile == nil {
+		t.Fatalf("expected service principal profile to be Empty and not nil for unmanaged identity, where client id and secret are not supplied in api model and deployment command")
+	}
+
+	// mockclient returns "app-id" for ClientID when empty
+	if deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID != "app-id" {
+		t.Fatalf("expected service principal profile client id to be empty but got %s", deployCmd.containerService.Properties.ServicePrincipalProfile.ClientID)
+	}
+
+	// mockcliet returns "client-secret" when empty
+	if deployCmd.containerService.Properties.ServicePrincipalProfile.Secret != "client-secret" {
+		t.Fatalf("expected service principal profile client secret to be empty but got %s", deployCmd.containerService.Properties.ServicePrincipalProfile.Secret)
+	}
+
+}
+
 func testAutodeployCredentialHandling(t *testing.T, useManagedIdentity bool, clientID, clientSecret string) {
 	apiloader := &api.Apiloader{
 		Translator: nil,
@@ -210,6 +297,7 @@ func testAutodeployCredentialHandling(t *testing.T, useManagedIdentity bool, cli
 		apimodelPath:    "./this/is/unused.json",
 		dnsPrefix:       "dnsPrefix1",
 		outputDirectory: "_test_output",
+		forceOverwrite:  true,
 		location:        "westus",
 
 		containerService: cs,
