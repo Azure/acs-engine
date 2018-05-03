f221be7c405969044c5e3f3ba3badfaf11bf5abb
Jack Francis <jack.francis@microsoft.com>
2018-05-03 14:42:16 -0700

    need to account for nil KubernetesConfig

--- pkg/api/convertertoapi.go
+++ pkg/api/convertertoapi.go
@@ -697,16 +697,19 @@ func setVlabsKubernetesDefaults(vp *vlabs.Properties, api *OrchestratorProfile) 
 	if api.KubernetesConfig == nil {
 		api.KubernetesConfig = &KubernetesConfig{}
 	}
-	// Included here for backwards compatibility with deprecated NetworkPolicy usage patterns
-	if vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy == NetworkPolicyAzure {
-		api.KubernetesConfig.NetworkPlugin = vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy
-		api.KubernetesConfig.NetworkPolicy = "" // no-op but included for emphasis
-	} else if vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy == NetworkPolicyNone {
-		api.KubernetesConfig.NetworkPlugin = NetworkPluginKubenet
-		api.KubernetesConfig.NetworkPolicy = "" // no-op but included for emphasis
-	} else {
-		api.KubernetesConfig.NetworkPlugin = vp.OrchestratorProfile.KubernetesConfig.NetworkPlugin
-		api.KubernetesConfig.NetworkPolicy = vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy
+
+	if vp.OrchestratorProfile.KubernetesConfig != nil {
+		// Included here for backwards compatibility with deprecated NetworkPolicy usage patterns
+		if vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy == NetworkPolicyAzure {
+			api.KubernetesConfig.NetworkPlugin = vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy
+			api.KubernetesConfig.NetworkPolicy = "" // no-op but included for emphasis
+		} else if vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy == NetworkPolicyNone {
+			api.KubernetesConfig.NetworkPlugin = NetworkPluginKubenet
+			api.KubernetesConfig.NetworkPolicy = "" // no-op but included for emphasis
+		} else {
+			api.KubernetesConfig.NetworkPlugin = vp.OrchestratorProfile.KubernetesConfig.NetworkPlugin
+			api.KubernetesConfig.NetworkPolicy = vp.OrchestratorProfile.KubernetesConfig.NetworkPolicy
+		}
 	}
 	if api.KubernetesConfig.NetworkPlugin == "" {
 		if vp.HasWindows() {
