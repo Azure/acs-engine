e3cda8a4b0725213a5106709d6fafa015ff730c5
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-05-09 13:34:12 -0700

    added URLs for Chinese cloud

--- parts/dcos/bstrap/bootstrapprovision.sh
+++ parts/dcos/bstrap/bootstrapprovision.sh
@@ -11,6 +11,14 @@ mkdir -p $TMPDIR
 LIBLTDL_DOWNLOAD_URL=https://dcos-mirror.azureedge.net/pkg/libltdl7_2.4.6-0.1_amd64.deb
 DOCKER_CE_DOWNLOAD_URL=https://dcos-mirror.azureedge.net/pkg/docker-ce_17.09.0~ce-0~ubuntu_amd64.deb
 
+case $DCOS_ENVIRONMENT in
+    # because of Chinese GreatWall Firewall, the default packages on Azure CDN is blocked. So the following Chinese local mirror url should be used instead.
+    AzureChinaCloud)
+        LIBLTDL_DOWNLOAD_URL=http://acsengine.blob.core.chinacloudapi.cn/dcos/libltdl7_2.4.6-0.1_amd64.deb
+        DOCKER_CE_DOWNLOAD_URL=http://acsengine.blob.core.chinacloudapi.cn/dcos/docker-ce_17.09.0~ce-0~ubuntu_amd64.deb
+    ;;
+esac
+
 curl -fLsSv --retry 20 -Y 100000 -y 60 -o $TMPDIR/1.deb $LIBLTDL_DOWNLOAD_URL &
 curl -fLsSv --retry 20 -Y 100000 -y 60 -o $TMPDIR/2.deb $DOCKER_CE_DOWNLOAD_URL &
 wait
