d1c1ebecf29f74ecef796365d6a164dae81eae81
Jack Francis <jack.francis@microsoft.com>
2018-05-14 13:57:34 -0700

    update ubuntu image to 16.04.201805090

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -65,7 +65,7 @@ var (
 		ImageOffer:     "UbuntuServer",
 		ImageSku:       "16.04-LTS",
 		ImagePublisher: "Canonical",
-		ImageVersion:   "16.04.201804270",
+		ImageVersion:   "16.04.201805090",
 	}
 
 	//DefaultRHELOSImageConfig is the RHEL Linux distribution.
