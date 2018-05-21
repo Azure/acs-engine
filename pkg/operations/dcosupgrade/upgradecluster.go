2bbc733d62a427c86cac6703869d5832314a1cea
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-05-21 10:48:36 -0700

    clean up the code

--- pkg/operations/dcosupgrade/upgradecluster.go
+++ pkg/operations/dcosupgrade/upgradecluster.go
@@ -1,13 +1,9 @@
 package dcosupgrade
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/Azure/acs-engine/pkg/api"
 	"github.com/Azure/acs-engine/pkg/armhelpers"
 	"github.com/Azure/acs-engine/pkg/i18n"
-	"github.com/Azure/azure-sdk-for-go/arm/compute"
 	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 )
@@ -21,14 +17,6 @@ type ClusterTopology struct {
 	CurrentDcosVersion string
 	NameSuffix         string
 	SSHKey             []byte
-}
-
-// AgentPoolTopology contains agent VMs in a single pool
-type AgentPoolTopology struct {
-	Identifier       *string
-	Name             *string
-	AgentVMs         *[]compute.VirtualMachine
-	UpgradedAgentVMs *[]compute.VirtualMachine
 }
 
 // UpgradeCluster upgrades a cluster with Orchestrator version X.X to version Y.Y.
@@ -50,40 +38,13 @@ func (uc *UpgradeCluster) UpgradeCluster(subscriptionID uuid.UUID, resourceGroup
 	uc.NameSuffix = nameSuffix
 	uc.SSHKey = sshKey
 
-	uc.Logger.Infof("Upgrading DCOS from %s to %s", uc.CurrentDcosVersion, uc.ClusterTopology.DataModel.Properties.OrchestratorProfile.OrchestratorVersion)
-
-	if err := uc.getClusterNodeStatus(subscriptionID, resourceGroup); err != nil {
-		return uc.Translator.Errorf("Error while querying ARM for resources: %+v", err)
-	}
-
 	upgradeVersion := uc.DataModel.Properties.OrchestratorProfile.OrchestratorVersion
-	uc.Logger.Infof("Upgrading to DCOS version %s", upgradeVersion)
+	uc.Logger.Infof("Upgrading DCOS from %s to %s", uc.CurrentDcosVersion, upgradeVersion)
 
 	if err := uc.runUpgrade(); err != nil {
 		return err
 	}
 
 	uc.Logger.Infof("Cluster upgraded successfully to DCOS %s", upgradeVersion)
-	return nil
-}
-
-func (uc *UpgradeCluster) getClusterNodeStatus(subscriptionID uuid.UUID, resourceGroup string) error {
-	vmListResult, err := uc.Client.ListVirtualMachines(resourceGroup)
-	if err != nil {
-		return err
-	}
-	bootstrapName := fmt.Sprintf("bootstrap-%s", uc.NameSuffix)
-	masterPrefix := fmt.Sprintf("dcos-master-%s-", uc.NameSuffix)
-
-	for _, vm := range *vmListResult.Value {
-
-		if strings.Compare(*(vm.Name), bootstrapName) == 0 {
-			uc.Logger.Infof("Bootstrap VM name: %s", *vm.Name)
-		} else if strings.HasPrefix(*(vm.Name), masterPrefix) {
-			uc.Logger.Infof("Master VM name: %s", *vm.Name)
-		} else {
-			uc.Logger.Infof("Agent VM name: %s", *vm.Name)
-		}
-	}
 	return nil
 }
