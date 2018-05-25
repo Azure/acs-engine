3a9bb9cf0c1c5bdce91dc9c8c1a00b995264e1ae
tariqibrahim <tariq1890@yahoo.in>
2018-05-24 22:37:37 -0700

    Updating the go-bindata go get links in the project.

--- pkg/acsengine/fileloader.go
+++ pkg/acsengine/fileloader.go
@@ -2,5 +2,5 @@ package acsengine
 
 //go:generate go-bindata -nometadata -pkg $GOPACKAGE -prefix ../../parts/ -o templates.go ../../parts/...
 //go:generate gofmt -s -l -w templates.go
-// fileloader use go-bindata (https://github.com/jteeuwen/go-bindata)
+// fileloader use go-bindata (https://github.com/go-bindata/go-bindata)
 // go-bindata is the way we handle embedded files, like binary, template, etc.
