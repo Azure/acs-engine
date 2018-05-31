a1fe7894a269d0d6eca9c0210b6a8593f4032876
Jack Francis <jackfrancis@gmail.com>
2018-05-31 10:16:01 -0700

    add support for k8s 1.11.0-alpha.2 (#2838)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -55,6 +55,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.10.2":         true,
 	"1.10.3":         true,
 	"1.11.0-alpha.1": true,
+	"1.11.0-alpha.2": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
@@ -220,7 +221,8 @@ func getAllKubernetesWindowsSupportedVersionsMap() map[string]bool {
 		"1.10.0-beta.2",
 		"1.10.0-beta.4",
 		"1.10.0-rc.1",
-		"1.11.0-alpha.1"} {
+		"1.11.0-alpha.1",
+		"1.11.0-alpha.2"} {
 		ret[version] = false
 	}
 	return ret
