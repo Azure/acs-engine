f84c1be4f6fc18af07040584481fb378ffdef9fa
CecileRobertMichon <cerobert@microsoft.com>
2018-06-01 14:32:59 -0700

    add 1.11.0-beta.1

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -57,6 +57,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.10.3":         true,
 	"1.11.0-alpha.1": true,
 	"1.11.0-alpha.2": true,
+	"1.11.0-beta.1":  true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
