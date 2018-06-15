6d0a9409ade8477d589d06273cede7ea7c42ab72
Cecile Robert-Michon <cerobert@microsoft.com>
2018-06-15 15:16:58 -0700

    Refactor validateDNSPrefix function in one place (#3276)

--- pkg/api/agentPoolOnlyApi/v20180331/validate.go
+++ pkg/api/agentPoolOnlyApi/v20180331/validate.go
@@ -101,7 +101,7 @@ func (a *Properties) Validate() error {
 
 	// Don't need to call validate.Struct(m)
 	// It is handled by Properties.Validate()
-	if e := validateDNSPrefix(a.DNSPrefix); e != nil {
+	if e := common.ValidateDNSPrefix(a.DNSPrefix); e != nil {
 		return e
 	}
 
@@ -144,18 +144,6 @@ func validatePoolName(poolName string) error {
 	submatches := re.FindStringSubmatch(poolName)
 	if len(submatches) != 2 {
 		return fmt.Errorf("pool name '%s' is invalid. A pool name must start with a lowercase letter, have max length of 12, and only have characters a-z0-9", poolName)
-	}
-	return nil
-}
-
-func validateDNSPrefix(dnsName string) error {
-	dnsNameRegex := `^([A-Za-z][A-Za-z0-9-]{1,43}[A-Za-z0-9])$`
-	re, err := regexp.Compile(dnsNameRegex)
-	if err != nil {
-		return err
-	}
-	if !re.MatchString(dnsName) {
-		return fmt.Errorf("DNSPrefix '%s' is invalid. The DNSPrefix must contain between 3 and 45 characters. The name can contain only letters, numbers, and hyphens.  The name must start with a letter and must end with a letter or a number. (length was %d)", dnsName, len(dnsName))
 	}
 	return nil
 }
