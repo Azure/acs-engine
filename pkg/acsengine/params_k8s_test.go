87158a44d6ac3d1879918d6de83b5062b604b599
Jack Francis <jack.francis@microsoft.com>
2018-06-15 10:19:51 -0700

    ensure params vals are bool, with test

---
+++ pkg/acsengine/params_k8s_test.go
@@ -0,0 +1,46 @@
+package acsengine
+
+import (
+	"path"
+	"testing"
+
+	"github.com/Azure/acs-engine/pkg/api"
+	"github.com/Azure/acs-engine/pkg/i18n"
+	"github.com/leonelquinteros/gotext"
+)
+
+func TestAssignKubernetesParameters(t *testing.T) {
+	// Initialize locale for translation
+	locale := gotext.NewLocale(path.Join("..", "..", "translations"), "en_US")
+	i18n.Initialize(locale)
+
+	apiloader := &api.Apiloader{
+		Translator: &i18n.Translator{
+			Locale: locale,
+		},
+	}
+	// iterate the test data directory
+	apiModelTestFiles := &[]APIModelTestFile{}
+	if e := IterateTestFilesDirectory(TestDataDir, apiModelTestFiles); e != nil {
+		t.Error(e.Error())
+		return
+	}
+
+	for _, tuple := range *apiModelTestFiles {
+		containerService, _, err := apiloader.LoadContainerServiceFromFile(tuple.APIModelFilename, true, false, nil)
+		if err != nil {
+			t.Errorf("Loading file %s got error: %s", tuple.APIModelFilename, err.Error())
+			continue
+		}
+
+		parametersMap := paramsMap{}
+		cloudSpecConfig := getCloudSpecConfig("eastus")
+		assignKubernetesParameters(containerService.Properties, parametersMap, cloudSpecConfig, DefaultGeneratorCode)
+		for k, v := range parametersMap {
+			switch val := v.(paramsMap)["value"].(type) {
+			case *bool:
+				t.Errorf("got a pointer to bool in paramsMap value, this is dangerous!: %s: %v", k, val)
+			}
+		}
+	}
+}
