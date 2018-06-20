3339bfb412a4fbd122a089a41d88578e09abe7e0
CecileRobertMichon <cerobert@microsoft.com>
2018-06-20 15:10:09 -0700

    remove flag test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -11,12 +11,6 @@ func TestNewRootCmd(t *testing.T) {
 	if output.Use != rootName || output.Short != rootShortDescription || output.Long != rootLongDescription {
 		t.Fatalf("root command should have use %s equal %s, short %s equal %s and long %s equal to %s", output.Use, rootName, output.Short, rootShortDescription, output.Long, rootLongDescription)
 	}
-	expectedFlags := []string{"debug"}
-	for _, f := range expectedFlags {
-		if output.Flags().Lookup(f) == nil {
-			t.Fatalf("root command should have flag %s, %v", f, output)
-		}
-	}
 	expectedCommands := []*cobra.Command{newDcosUpgradeCmd(), newDeployCmd(), newGenerateCmd(), newOrchestratorsCmd(), newScaleCmd(), newUpgradeCmd(), newVersionCmd()}
 	rc := output.Commands()
 	for i, c := range expectedCommands {
