de9c2b62d5dc8c732a021e9b352f8655d1352772
Patrick Lang <plang@microsoft.com>
2018-06-21 12:24:36 -0700

    Adding timestamps to logs

--- test/e2e/kubernetes/util/util.go
+++ test/e2e/kubernetes/util/util.go
@@ -2,11 +2,23 @@ package util
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 // PrintCommand prints a command string
 func PrintCommand(cmd *exec.Cmd) {
 	fmt.Printf("\n$ %s\n", strings.Join(cmd.Args, " "))
+}
+
+func RunAndLogCommand(cmd *exec.Cmd) ([]byte, error) {
+	cmdLine := fmt.Sprintf("\n$ %s\n", strings.Join(cmd.Args, " "))
+	start := time.Now()
+	log.Printf("%s #### Started at %s", cmdLine, start.String())
+	out, err := cmd.CombinedOutput()
+	end := time.Now()
+	log.Printf("\n#### %s completed in %s", end.Sub(start).String())
+	return out, err
 }
