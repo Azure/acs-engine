938004c0c1efef0da9ed234cc157421744e0ded4
Jack Francis <jackfrancis@gmail.com>
2018-06-26 16:28:09 -0700

    enable v1.11.0-rc.3 (#3376)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -65,6 +65,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.11.0-beta.2":  true,
 	"1.11.0-rc.1":    true,
 	"1.11.0-rc.2":    true,
+	"1.11.0-rc.3":    true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
