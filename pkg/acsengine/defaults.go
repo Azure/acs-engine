8970a45865a3eb3a6857d05979111cdb91d9a665
Jack Francis <jack.francis@microsoft.com>
2018-07-05 16:27:15 -0700

    Azure CNI v1.0.7

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -22,7 +22,7 @@ const (
 	// AzureCniPluginVer specifies version of Azure CNI plugin, which has been mirrored from
 	// https://github.com/Azure/azure-container-networking/releases/download/${AZURE_PLUGIN_VER}/azure-vnet-cni-linux-amd64-${AZURE_PLUGIN_VER}.tgz
 	// to https://acs-mirror.azureedge.net/cni/
-	AzureCniPluginVer = "v1.0.4"
+	AzureCniPluginVer = "v1.0.7"
 	// CNIPluginVer specifies the version of CNI implementation
 	// https://github.com/containernetworking/plugins
 	CNIPluginVer = "v0.7.1"
