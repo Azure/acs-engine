2c7deef95988030040f46f9f4d01b6417efd427d
Jack Francis <jackfrancis@gmail.com>
2018-07-18 12:37:00 -0700

    add support for k8s v1.11.1 (#3495)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -69,6 +69,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.11.0-rc.2":    true,
 	"1.11.0-rc.3":    true,
 	"1.11.0":         true,
+	"1.11.1":         true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
