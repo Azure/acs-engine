19be13545ad45ce41e9637f5eb0198f4bd93c521
tariq1890 <tariq181290@gmail.com>
2018-08-01 00:20:33 -0700

    Parallelizing all validate test cases

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -71,7 +71,7 @@ func PointerToInt(i int) *int {
 	return &p
 }
 
-// EqualError is a ni;-safe method which reports whether errors a and b are considered equal.
+// EqualError is a nil-safe method which reports whether errors a and b are considered equal.
 // They're equal if both are nil, or both are not nil and a.Error() == b.Error().
 func EqualError(a, b error) bool {
 	return a == nil && b == nil || a != nil && b != nil && a.Error() == b.Error()
