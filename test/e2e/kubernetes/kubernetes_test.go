1b35ed20f7cf259ac3364714e309707dd08248ff
rashmy <rashmy@RASHMY-PC2>
2018-08-02 11:44:41 -0700

    updating omsagent log tests

--- test/e2e/kubernetes/kubernetes_test.go
+++ test/e2e/kubernetes/kubernetes_test.go
@@ -410,7 +410,7 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				By("Ensuring that the kubepodinventory plugin is writing data successfully")
 				pods, err := pod.GetAllByPrefix("omsagent-rs", "kube-system")
 				Expect(err).NotTo(HaveOccurred())
-				_, err = pods[0].Exec("grep", "\"in_kube_podinventory::emit-stream : Success\"", "/var/opt/microsoft/omsagent/log/omsagent.log")
+				_, err = pods[0].ValidateOmsAgentLogs("\"in_kube_podinventory::emit-stream : Success\"", 30*time.Second, cfg.Timeout)
 				Expect(err).NotTo(HaveOccurred())
 			} else {
 				Skip("container monitoring disabled for this cluster, will not test")
@@ -425,7 +425,7 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				By("Ensuring that the kubenodeinventory plugin is writing data successfully")
 				pods, err := pod.GetAllByPrefix("omsagent-rs", "kube-system")
 				Expect(err).NotTo(HaveOccurred())
-				_, err = pods[0].Exec("grep", "\"in_kube_nodeinventory::emit-stream : Success\"", "/var/opt/microsoft/omsagent/log/omsagent.log")
+				_, err = pods[0].ValidateOmsAgentLogs("\"in_kube_nodeinventory::emit-stream : Success\"", 30*time.Second, cfg.Timeout)
 				Expect(err).NotTo(HaveOccurred())
 			} else {
 				Skip("container monitoring disabled for this cluster, will not test")
@@ -440,7 +440,7 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 				By("Ensuring that the cadvisor_perf plugin is writing data successfully")
 				pods, err := pod.GetAllByPrefix("omsagent", "kube-system")
 				Expect(err).NotTo(HaveOccurred())
-				_, err = pods[0].Exec("grep", "\"in_cadvisor_perf::emit-stream : Success\"", "/var/opt/microsoft/omsagent/log/omsagent.log")
+				_, err = pods[0].ValidateOmsAgentLogs("\"in_cadvisor_perf::emit-stream : Success\"", 30*time.Second, cfg.Timeout)
 				Expect(err).NotTo(HaveOccurred())
 			} else {
 				Skip("container monitoring disabled for this cluster, will not test")
