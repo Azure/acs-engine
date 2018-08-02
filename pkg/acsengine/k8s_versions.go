bdb0ef84362b37014f431f432b2317fde382450f
Jack Francis <jack.francis@microsoft.com>
2018-08-02 14:44:13 -0700

    update cluster-autoscaler for 1.12
    See:
    
    https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md#v1120-alpha1

--- pkg/acsengine/k8s_versions.go
+++ pkg/acsengine/k8s_versions.go
@@ -23,7 +23,7 @@ var k8sComponentVersions = map[string]map[string]string{
 		"aci-connector":                    "virtual-kubelet:latest",
 		ContainerMonitoringAddonName:       "oms:ciprod05082018",
 		AzureCNINetworkMonitoringAddonName: "networkmonitor:v0.0.4",
-		"cluster-autoscaler":               "cluster-autoscaler:v1.3.0",
+		"cluster-autoscaler":               "cluster-autoscaler:v1.3.1",
 		NVIDIADevicePluginAddonName:        "k8s-device-plugin:1.11",
 		"k8s-dns-sidecar":                  "k8s-dns-sidecar-amd64:1.14.10",
 		"nodestatusfreq":                   DefaultKubernetesNodeStatusUpdateFrequency,
