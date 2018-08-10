f31e0e013db47226fea8508944f37eba97d48a1e
Cecile Robert-Michon <cerobert@microsoft.com>
2018-08-10 13:27:24 -0700

    az-login as a dependency

--- packer.mk
+++ packer.mk
@@ -7,11 +7,11 @@ init-packer:
 az-login:
 	az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
 
-run-packer:
-	@packer version && make az-login && make init-packer && (make build-packer | tee packer-output)
+run-packer: az-login
+	@packer version && make init-packer && (make build-packer | tee packer-output)
 
-az-copy:
-	@make az-login && (azcopy --source "${OS_DISK_SAS}" --destination "${CLASSIC_BLOB}/${VHD_NAME}" --dest-sas "${CLASSIC_SAS_TOKEN}")
+az-copy: az-login
+	azcopy --source "${OS_DISK_SAS}" --destination "${CLASSIC_BLOB}/${VHD_NAME}" --dest-sas "${CLASSIC_SAS_TOKEN}"
 
-generate-sas:
-	@make az-login && (az storage container generate-sas --name vhds --permissions lr --connection-string "${CLASSIC_SA_CONNECTION_STRING}" --start ${START_DATE} --expiry ${EXPIRY_DATE} | tee vhd-sas)
+generate-sas: az-login
+	az storage container generate-sas --name vhds --permissions lr --connection-string "${CLASSIC_SA_CONNECTION_STRING}" --start ${START_DATE} --expiry ${EXPIRY_DATE} | tee vhd-sas)
