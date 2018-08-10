1a57d829ea588891bcb3d0e8014b8d958fa416f4
Cecile Robert-Michon <cerobert@microsoft.com>
2018-08-10 13:55:33 -0700

    update packer.mk

--- packer.mk
+++ packer.mk
@@ -1,17 +1,17 @@
 build-packer:
-	@packer build -var-file=packer/settings.json packer/vhd-image.json
+	@packer build -var-file=packer/settings.json packer/vhd-image-builder.json
 
 init-packer:
-	@./packer/scripts/init-variables
+	@./packer/init-variables
 
 az-login:
 	az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
 
-run-packer:
-	@packer version && make az-login && make init-packer && (make build-packer | tee packer-output)
+run-packer: az-login
+	@packer version && make init-packer && (make build-packer | tee packer-output)
 
-az-copy:
-	@make az-login && (azcopy --source "${OS_DISK_SAS}" --destination "${CLASSIC_BLOB}/${VHD_NAME}" --dest-sas "${CLASSIC_SAS_TOKEN}")
+az-copy: az-login
+	azcopy --source "${OS_DISK_SAS}" --destination "${CLASSIC_BLOB}/${VHD_NAME}" --dest-sas "${CLASSIC_SAS_TOKEN}"
 
-generate-sas:
-	@make az-login && (az storage container generate-sas --name vhds --permissions lr --connection-string "${CLASSIC_SA_CONNECTION_STRING}" --start ${START_DATE} --expiry ${EXPIRY_DATE} | tee vhd-sas)
+generate-sas: az-login
+	az storage container generate-sas --name vhds --permissions lr --connection-string "${CLASSIC_SA_CONNECTION_STRING}" --start ${START_DATE} --expiry ${EXPIRY_DATE} | tee vhd-sas)
