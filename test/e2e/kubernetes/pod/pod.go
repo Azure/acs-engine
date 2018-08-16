ee8480b6c1f8259caf2b8ac1a114a24e215e5bf3
Jack Francis <jack.francis@microsoft.com>
2018-08-16 11:51:31 -0700

    print output

--- test/e2e/kubernetes/pod/pod.go
+++ test/e2e/kubernetes/pod/pod.go
@@ -140,6 +140,7 @@ func RunLinuxPod(image, name, namespace, command string) (*Pod, error) {
 	overrides := `{ "spec":{"template":{"spec": {"nodeSelector":{"beta.kubernetes.io/os":"linux"}}}}}`
 	cmd := exec.Command("kubectl", "run", name, "-n", namespace, "--image", image, "--image-pull-policy=IfNotPresent", "--restart=Never", "--overrides", overrides, "--command", "--", "/bin/sh", "-c", command)
 	out, err := util.RunAndLogCommand(cmd)
+	fmt.Printf("%s\n", string(out[:]))
 	if err != nil {
 		log.Printf("Error trying to deploy %s [%s] in namespace %s:%s\n", name, image, namespace, string(out))
 		return nil, err
