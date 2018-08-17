28621cae821472b6695da2fb017af770cac2d946
James Sturtevant <jstur@microsoft.com>
2018-08-17 15:02:06 -0700

    add windows dns liveness check

--- test/e2e/kubernetes/kubernetes_test.go
+++ test/e2e/kubernetes/kubernetes_test.go
@@ -261,7 +261,7 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 			}
 		})
 
-		It("should be able to launch a long-running container networking DNS liveness pod", func() {
+		FIt("should be able to launch a long-running container networking DNS liveness pod", func() {
 			if !eng.HasNetworkPolicy("calico") {
 				var err error
 				var p *pod.Pod
@@ -275,6 +275,21 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 					}
 				}
 				running, err := p.WaitOnReady(5*time.Second, 2*time.Minute)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(running).To(Equal(true))
+			}
+
+			if eng.HasWindowsAgents() && !eng.HasNetworkPolicy("calico") {
+				windowsPod, err := pod.CreatePodFromFile(filepath.Join(WorkloadDir, "dns-liveness-windows.yaml"), "dns-liveness-windows", "default", 1*time.Second, cfg.Timeout)
+				if cfg.SoakClusterName == "" {
+					Expect(err).NotTo(HaveOccurred())
+				} else {
+					if err != nil {
+						windowsPod, err = pod.Get("dns-liveness-windows", "default")
+						Expect(err).NotTo(HaveOccurred())
+					}
+				}
+				running, err := windowsPod.WaitOnReady(5*time.Second, 2*time.Minute)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(running).To(Equal(true))
 			} else {
@@ -1287,8 +1302,9 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 	})
 
 	Describe("after the cluster has been up for awhile", func() {
-		It("dns-liveness pod should not have any restarts", func() {
+		FIt("dns-liveness pod should not have any restarts", func() {
 			if !eng.HasNetworkPolicy("calico") {
+				By("by checking restarts on pod")
 				pod, err := pod.Get("dns-liveness", "default")
 				Expect(err).NotTo(HaveOccurred())
 				running, err := pod.WaitOnReady(5*time.Second, 3*time.Minute)
@@ -1301,6 +1317,23 @@ var _ = Describe("Azure Container Cluster using the Kubernetes Orchestrator", fu
 					Expect(restarts).To(Equal(0))
 				} else {
 					log.Printf("%d DNS livenessProbe restarts since this cluster was created...\n", restarts)
+				}
+			}
+
+			if eng.HasWindowsAgents() && !eng.HasNetworkPolicy("calico") {
+				By("by checking restarts on windows pod")
+				windowspod, err := pod.Get("dns-liveness-windows", "default")
+				Expect(err).NotTo(HaveOccurred())
+				running, err := windowspod.WaitOnReady(5*time.Second, 3*time.Minute)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(running).To(Equal(true))
+				restarts := windowspod.Status.ContainerStatuses[0].RestartCount
+				if cfg.SoakClusterName == "" {
+					err = windowspod.Delete(deleteResourceRetries)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(restarts).To(Equal(0))
+				} else {
+					log.Printf("%d Windows DNS livenessProbe restarts since this cluster was created...\n", restarts)
 				}
 			} else {
 				Skip("We don't run DNS liveness checks on calico clusters ( //TODO )")
