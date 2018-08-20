75abecc70fc7c47482e82f3c2b3ea50794a49e57
Rita Zhang <rita.z.zhang@gmail.com>
2018-08-20 13:37:24 -0700

    k8s 1.12 issue workaround (#3684)
    * k8s 1.12 issue workaround
    
    * conditional to 1.12

--- parts/k8s/kubernetescustomscript.sh
+++ parts/k8s/kubernetescustomscript.sh
@@ -108,6 +108,10 @@ if [[ ! -z "${MASTER_NODE}" ]]; then
     writeKubeConfig
     ensureEtcd
     ensureK8sControlPlane
+    # workaround for 1.12 bug https://github.com/Azure/acs-engine/issues/3681 will remove once upstream is fixed
+    if [[ "${KUBERNETES_VERSION}" = 1.12.* ]]; then
+        ensureKubelet 
+    fi
 fi
 
 if [[ $OS == $UBUNTU_OS_NAME ]]; then
