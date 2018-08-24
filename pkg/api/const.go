5fb29a59a235c285403a303f6645d8c85036e5ca
Jack Francis <jack.francis@microsoft.com>
2018-08-24 10:18:55 -0700

    comment

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -172,4 +172,5 @@ const (
 	AgentPoolProfileRoleMaster AgentPoolProfileRole = "master"
 )
 
+// DefaultARMOperationTimeout defines a default (permissive) ARM operation timeout
 const DefaultARMOperationTimeout = 150 * time.Minute
