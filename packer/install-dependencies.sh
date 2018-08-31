2ed19c02d5cb33a1cfed003f999f3e5306b4a850
Jack Francis <jack.francis@microsoft.com>
2018-08-31 13:51:11 -0700

    do the new stuff first

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -33,14 +33,6 @@ installContainerd
 
 installImg
 
-# TODO: fetch supported k8s versions from an acs-engine command instead of hardcoding them here
-K8S_VERSIONS="1.7.7 1.7.9 1.7.12 1.7.15 1.7.16 1.8.0 1.8.1 1.8.2 1.8.4 1.8.5 1.8.6 1.8.7 1.8.8 1.8.9 1.8.10 1.8.11 1.8.12 1.8.13 1.8.14 1.8.15 1.9.0 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.9.7 1.9.8 1.9.9 1.9.10 1.10.0 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 1.10.7 1.11.0 1.11.1 1.11.2"
-
-for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
-    HYPERKUBE_URL="k8s.gcr.io/hyperkube-amd64:v${KUBERNETES_VERSION}"
-    pullHyperkube
-done
-
 DASHBOARD_VERSIONS="1.8.3 1.6.3"
 for DASHBOARD_VERSION in ${DASHBOARD_VERSIONS}; do
     pullImg "k8s.gcr.io/kubernetes-dashboard-amd64:v${DASHBOARD_VERSION}"
@@ -123,9 +115,16 @@ done
 
 pullImg "busybox"
 
+# TODO: fetch supported k8s versions from an acs-engine command instead of hardcoding them here
+K8S_VERSIONS="1.7.7 1.7.9 1.7.12 1.7.15 1.7.16 1.8.0 1.8.1 1.8.2 1.8.4 1.8.5 1.8.6 1.8.7 1.8.8 1.8.9 1.8.10 1.8.11 1.8.12 1.8.13 1.8.14 1.8.15 1.9.0 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.9.7 1.9.8 1.9.9 1.9.10 1.10.0 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 1.10.7 1.11.0 1.11.1 1.11.2"
+
+for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
+    HYPERKUBE_URL="k8s.gcr.io/hyperkube-amd64:v${KUBERNETES_VERSION}"
+    pullHyperkube
+done
+
 df -h
 
-echo "Install completed successfully on " `date` > /var/log/azure/golden-image-install.complete
-echo "\nVSTS Build NUMBER: ${BUILD_NUMBER}" >> /var/log/azure/golden-image-install.complete
+echo "Install completed successfully on " `date` > /var/log/azure/golden-image-install.completeecho "\nVSTS Build NUMBER: ${BUILD_NUMBER}" >> /var/log/azure/golden-image-install.complete
 echo "\nVSTS Build ID: ${BUILD_ID}" >> /var/log/azure/golden-image-install.complete
 echo "\nCommit: ${COMMIT}" >> /var/log/azure/golden-image-install.complete
