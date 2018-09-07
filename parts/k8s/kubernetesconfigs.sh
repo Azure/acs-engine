a742457dadb3067a847d48be5d2d37b8e93f807e
Matt Boersma <Matt.Boersma@microsoft.com>
2018-09-07 10:37:34 -0600

    Enable health-monitor.sh for kubelet

--- parts/k8s/kubernetesconfigs.sh
+++ parts/k8s/kubernetesconfigs.sh
@@ -258,10 +258,9 @@ function ensureKubelet() {
     KUBELET_RUNTIME_CONFIG_SCRIPT_FILE=/opt/azure/containers/kubelet.sh
     wait_for_file 1200 1 $KUBELET_RUNTIME_CONFIG_SCRIPT_FILE || exit $ERR_FILE_WATCH_TIMEOUT
     systemctlEnableAndStart kubelet
-    # Don't start the kubelet-health-monitor systemd unit (yet).
-    # KUBELET_HEALTH_MONITOR_SYSTEMD_FILE=/etc/systemd/system/kubelet-health-monitor.service
-    # wait_for_file 1200 1 $KUBELET_HEALTH_MONITOR_SYSTEMD_FILE || exit $ERR_FILE_WATCH_TIMEOUT
-    # systemctlEnableAndStart kubelet-health-monitor
+    KUBELET_HEALTH_MONITOR_SYSTEMD_FILE=/etc/systemd/system/kubelet-health-monitor.service
+    wait_for_file 1200 1 $KUBELET_HEALTH_MONITOR_SYSTEMD_FILE || exit $ERR_FILE_WATCH_TIMEOUT
+    systemctlEnableAndStart kubelet-health-monitor
 }
 
 function ensureJournal(){
