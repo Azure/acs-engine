948d7bb347ccf330aa6a879361e52d346c6ce8ba
Cecile Robert-Michon <cerobert@microsoft.com>
2018-09-09 11:51:51 -0700

    re-add absolute paths

--- parts/k8s/kubernetesinstalls.sh
+++ parts/k8s/kubernetesinstalls.sh
@@ -36,7 +36,6 @@ function installGPUDrivers() {
     echo blacklist nouveau >> /etc/modprobe.d/blacklist.conf
     update-initramfs -u
     mkdir -p $GPU_DEST
-    cd $GPU_DEST
     # Installing nvidia-docker, setting nvidia runtime as default and restarting docker daemon
     retrycmd_if_failure_no_stats 180 1 5 curl -fsSL https://nvidia.github.io/nvidia-docker/gpgkey > /tmp/aptnvidia.gpg || exit $ERR_GPU_DRIVERS_INSTALL_TIMEOUT
     cat /tmp/aptnvidia.gpg | apt-key add -
@@ -46,14 +45,12 @@ function installGPUDrivers() {
     retrycmd_if_failure 30 5 300 apt-get install -y linux-headers-$(uname -r) gcc make dkms || exit $ERR_GPU_DRIVERS_INSTALL_TIMEOUT
     retrycmd_if_failure 30 5 300 apt-get -o Dpkg::Options::="--force-confold" install -y nvidia-docker2=$NVIDIA_DOCKER_VERSION+docker$DOCKER_VERSION nvidia-container-runtime=$NVIDIA_CONTAINER_RUNTIME_VERSION+docker$DOCKER_VERSION || exit $ERR_GPU_DRIVERS_INSTALL_TIMEOUT
     pkill -SIGHUP dockerd
-    mkdir -p $GPU_DEST
-    cd $GPU_DEST
     # Download the .run file from NVIDIA.
     # Nvidia libraries are always install in /usr/lib/x86_64-linux-gnu, and there is no option in the run file to change this.
     # Instead we use Overlayfs to move the newly installed libraries under /usr/local/nvidia/lib64
-    retrycmd_if_failure 5 10 60 curl -fLS https://us.download.nvidia.com/tesla/$GPU_DV/NVIDIA-Linux-x86_64-$GPU_DV.run -o nvidia-drivers-$GPU_DV || exit $ERR_GPU_DRIVERS_INSTALL_TIMEOUT
-    mkdir -p lib64 overlay-workdir
-    mount -t overlay -o lowerdir=/usr/lib/x86_64-linux-gnu,upperdir=lib64,workdir=overlay-workdir none /usr/lib/x86_64-linux-gnu
+    retrycmd_if_failure 5 10 60 curl -fLS https://us.download.nvidia.com/tesla/$GPU_DV/NVIDIA-Linux-x86_64-$GPU_DV.run -o $GPU_DEST/nvidia-drivers-$GPU_DV || exit $ERR_GPU_DRIVERS_INSTALL_TIMEOUT
+    mkdir -p $GPU_DEST/lib64 $GPU_DEST/overlay-workdir
+    mount -t overlay -o lowerdir=/usr/lib/x86_64-linux-gnu,upperdir=$GPU_DEST/lib64,workdir=$GPU_DEST/overlay-workdir none /usr/lib/x86_64-linux-gnu
 }
 
 function installContainerRuntime() {
