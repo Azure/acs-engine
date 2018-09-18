56c41be89e049e7e53498dde33f287ff4849640e
Matt Boersma <Matt.Boersma@microsoft.com>
2018-09-18 16:24:10 -0600

    add support for Kubernetes 1.10.8
    See https://github.com/kubernetes/kubernetes/releases/tag/v1.10.8

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -118,7 +118,7 @@ done
 pullContainerImage "docker" "busybox"
 
 # TODO: fetch supported k8s versions from an acs-engine command instead of hardcoding them here
-K8S_VERSIONS="1.7.15 1.7.16 1.8.14 1.8.15 1.9.9 1.9.10 1.10.6 1.10.7 1.11.2 1.11.3"
+K8S_VERSIONS="1.7.15 1.7.16 1.8.14 1.8.15 1.9.9 1.9.10 1.10.7 1.10.8 1.11.2 1.11.3"
 
 for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
     HYPERKUBE_URL="k8s.gcr.io/hyperkube-amd64:v${KUBERNETES_VERSION}"
