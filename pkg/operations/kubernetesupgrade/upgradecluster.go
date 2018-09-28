13a0baf5df8e9790ef7538951bb259761d571358
Matt Boersma <Matt.Boersma@microsoft.com>
2018-09-28 14:08:18 -0600

    Add upgrader clause for k8s 1.12.x

--- pkg/operations/kubernetesupgrade/upgradecluster.go
+++ pkg/operations/kubernetesupgrade/upgradecluster.go
@@ -121,6 +121,11 @@ func (uc *UpgradeCluster) UpgradeCluster(subscriptionID uuid.UUID, kubeConfig, r
 		u.Init(uc.Translator, uc.Logger, uc.ClusterTopology, uc.Client, kubeConfig, uc.StepTimeout, acsengineVersion)
 		upgrader = u
 
+	case strings.HasPrefix(upgradeVersion, "1.12."):
+		upgrader112 := &Upgrader{}
+		upgrader112.Init(uc.Translator, uc.Logger, uc.ClusterTopology, uc.Client, kubeConfig, uc.StepTimeout, acsengineVersion)
+		upgrader = upgrader112
+
 	default:
 		return uc.Translator.Errorf("Upgrade to Kubernetes version %s is not supported", upgradeVersion)
 	}
