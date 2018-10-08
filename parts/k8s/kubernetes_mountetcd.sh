d96914341903c9b1301c6dbd103941ad9cc093d4
Jack Francis <jackfrancis@gmail.com>
2018-10-08 10:55:34 -0700

    optimize customData payload by removing comments (#3971)

--- parts/k8s/kubernetes_mountetcd.sh
+++ parts/k8s/kubernetes_mountetcd.sh
@@ -13,17 +13,14 @@ then
     echo "disk is already mounted"
     exit 0
 fi
-# fill /etc/fstab
 grep "/dev/sdc1" /etc/fstab
 if [ $? -ne 0 ]
 then
     echo "$PARTITION       $MOUNTPOINT       auto    defaults,nofail       0       2" >> /etc/fstab
 fi
-# check if partition exists
 ls $PARTITION
 if [ $? -ne 0 ]
 then
-    # partition does not exist
     /sbin/sgdisk --new 1 $DISK
     /sbin/mkfs.ext4 $PARTITION -L etcd_disk -F -E lazy_itable_init=1,lazy_journal_init=1
 fi
