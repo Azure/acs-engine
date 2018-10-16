5ce70d359657d6a0faaa3faa5b3c9f91436bf0d5
Matt Boersma <Matt.Boersma@microsoft.com>
2018-10-16 13:09:33 -0600

    Add support for Kubernetes v1.13.0-alpha.1 (#4036)

--- pkg/api/common/versions.go
+++ pkg/api/common/versions.go
@@ -86,6 +86,7 @@ var AllKubernetesSupportedVersions = map[string]bool{
 	"1.12.0-rc.2":    false,
 	"1.12.0":         true,
 	"1.12.1":         true,
+	"1.13.0-alpha.1": true,
 }
 
 // GetDefaultKubernetesVersion returns the default Kubernetes version, that is the latest patch of the default release
