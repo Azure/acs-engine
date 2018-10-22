1ff1113232680093fa968f7d948eda377f0c4da7
Tariq Ibrahim <tariq181290@gmail.com>
2018-10-22 09:14:09 -0700

    Fix issue where kubernetesDashboard params weren't being added despite e enabling the dashboard addon (#4084)

--- pkg/acsengine/params_k8s_test.go
+++ pkg/acsengine/params_k8s_test.go
@@ -4,6 +4,8 @@ import (
 	"path"
 	"testing"
 
+	"github.com/Azure/acs-engine/pkg/helpers"
+
 	"github.com/Azure/acs-engine/pkg/api"
 	"github.com/Azure/acs-engine/pkg/i18n"
 	"github.com/leonelquinteros/gotext"
@@ -44,4 +46,130 @@ func TestAssignKubernetesParameters(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestKubernetesParamsAddons(t *testing.T) {
+	tests := map[string]struct {
+		addon          api.KubernetesAddon
+		expectedParams []string
+	}{
+		"nvidia-device-plugin": {
+			api.KubernetesAddon{
+				Name:    api.NVIDIADevicePluginAddonName,
+				Enabled: helpers.PointerToBool(true),
+				Containers: []api.KubernetesContainerSpec{
+					{
+						Name:           api.NVIDIADevicePluginAddonName,
+						CPURequests:    "50m",
+						MemoryRequests: "150Mi",
+						CPULimits:      "50m",
+						MemoryLimits:   "150Mi",
+					},
+				},
+			},
+			[]string{
+				"kubernetesNVIDIADevicePluginCPURequests",
+				"kubernetesNVIDIADevicePluginMemoryRequests",
+				"kubernetesNVIDIADevicePluginCPULimit",
+				"kubernetesNVIDIADevicePluginMemoryLimit",
+				"kubernetesNVIDIADevicePluginSpec",
+			},
+		},
+
+		"container-monitoring": {
+			api.KubernetesAddon{
+				Name:    api.ContainerMonitoringAddonName,
+				Enabled: helpers.PointerToBool(true),
+				Containers: []api.KubernetesContainerSpec{
+					{
+						Name:           "omsagent",
+						CPURequests:    "50m",
+						MemoryRequests: "150Mi",
+						CPULimits:      "50m",
+						MemoryLimits:   "150Mi",
+					},
+				},
+			},
+			[]string{
+				"omsAgentVersion",
+				"omsAgentDockerProviderVersion",
+				"omsAgentWorkspaceGuid",
+				"omsAgentWorkspaceKey",
+				"kubernetesOMSAgentCPURequests",
+				"kubernetesOMSAgentCPULimit",
+				"kubernetesOMSAgentMemoryLimit",
+				"kubernetesOMSAgentMemoryRequests",
+				"omsAgentImage",
+			},
+		},
+
+		"kubernetes-dashboard": {
+			api.KubernetesAddon{
+				Name:    api.DefaultDashboardAddonName,
+				Enabled: helpers.PointerToBool(true),
+				Containers: []api.KubernetesContainerSpec{
+					{
+						Name:           api.DefaultDashboardAddonName,
+						CPURequests:    "50m",
+						MemoryRequests: "150Mi",
+						CPULimits:      "50m",
+						MemoryLimits:   "150Mi",
+					},
+				},
+			},
+			[]string{
+				"kubernetesDashboardCPURequests",
+				"kubernetesDashboardCPULimit",
+				"kubernetesDashboardMemoryRequests",
+				"kubernetesDashboardMemoryLimit",
+				"kubernetesDashboardSpec",
+			},
+		},
+
+		"rescheduler": {
+			api.KubernetesAddon{
+				Name:    api.DefaultReschedulerAddonName,
+				Enabled: helpers.PointerToBool(true),
+				Containers: []api.KubernetesContainerSpec{
+					{
+						Name:           api.DefaultReschedulerAddonName,
+						CPURequests:    "50m",
+						MemoryRequests: "150Mi",
+						CPULimits:      "50m",
+						MemoryLimits:   "150Mi",
+					},
+				},
+			},
+			[]string{
+				"kubernetesReschedulerCPURequests",
+				"kubernetesReschedulerCPULimit",
+				"kubernetesReschedulerMemoryRequests",
+				"kubernetesReschedulerMemoryLimit",
+				"kubernetesReschedulerSpec",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			properties := api.GetK8sDefaultProperties(false)
+			properties.OrchestratorProfile.KubernetesConfig.Addons = []api.KubernetesAddon{test.addon}
+			parametersMap := paramsMap{}
+			assignKubernetesParameters(properties, parametersMap, api.AzureCloudSpec, DefaultGeneratorCode)
+
+			for _, expectedParam := range test.expectedParams {
+				if !isKeyPresent(expectedParam, parametersMap) {
+					t.Errorf("expected key %s to be present in the map", expectedParam)
+				}
+			}
+		})
+	}
+
+}
+
+func isKeyPresent(key string, paramMap map[string]interface{}) bool {
+	_, ok := paramMap[key]
+	return ok
 }
