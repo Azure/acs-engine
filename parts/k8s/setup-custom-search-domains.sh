1f210591d53567258512f2f9f8909b729e5fea69
Jack Francis <jackfrancis@gmail.com>
2018-10-22 13:47:54 -0700

    apt lock hygiene (#4081)

--- parts/k8s/setup-custom-search-domains.sh
+++ parts/k8s/setup-custom-search-domains.sh
@@ -4,5 +4,7 @@ source /opt/azure/containers/provision_source.sh
 
 sudo echo "  dns-search <searchDomainName>" >> /etc/network/interfaces.d/50-cloud-init.cfg
 systemctl_restart 20 5 10 restart networking
+wait_for_apt_locks
 retrycmd_if_failure 10 5 120 apt-get -y install realmd sssd sssd-tools samba-common samba samba-common python2.7 samba-libs packagekit
+wait_for_apt_locks
 echo "<searchDomainRealmPassword>" | realm join -U <searchDomainRealmUser>@`echo "<searchDomainName>" | tr /a-z/ /A-Z/` `echo "<searchDomainName>" | tr /a-z/ /A-Z/`
