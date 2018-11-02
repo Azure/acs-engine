3a3f134d9422479c29cc0585c959922b4a8067e7
Jack Francis <jack.francis@microsoft.com>
2018-11-02 13:55:33 -0700

    comments

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -382,7 +382,9 @@ const (
 )
 
 const (
-	AzurePublicCloud       = "AzurePublicCloud"
+	// AzurePublicCloud is a const string reference identifier for public cloud
+	AzurePublicCloud = "AzurePublicCloud"
+	// AzureChinaCloud is a const string reference identifier for china cloud
 	AzureChinaCloud        = "AzureChinaCloud"
 	azureGermanCloud       = "AzureGermanCloud"
 	azureUSGovernmentCloud = "AzureUSGovernmentCloud"
