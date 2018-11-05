88a2c6e8da7691bddc1a886af6ebb93d641d91b1
Jack Francis <jack.francis@microsoft.com>
2018-11-05 08:33:12 -0800

    fix validation test, pass networkPolicy to CSE

--- parts/k8s/kubernetesconfigs.sh
+++ parts/k8s/kubernetesconfigs.sh
@@ -186,10 +186,10 @@ configureCNI() {
     retrycmd_if_failure 120 5 25 modprobe br_netfilter || exit $ERR_MODPROBE_FAIL
     echo -n "br_netfilter" > /etc/modules-load.d/br_netfilter.conf
     if [[ "${NETWORK_PLUGIN}" = "azure" ]]; then
-	# Casey TODO: When using Calico with Azure networking, we don't want this CNI
-	# config - Calico will install its own. How do we make this function aware of that?
-        # mv $CNI_BIN_DIR/10-azure.conflist $CNI_CONFIG_DIR/
-        # chmod 600 $CNI_CONFIG_DIR/10-azure.conflist
+        if [[ "${NETWORK_POLICY}" != "calico" ]]; then
+            mv $CNI_BIN_DIR/10-azure.conflist $CNI_CONFIG_DIR/
+            chmod 600 $CNI_CONFIG_DIR/10-azure.conflist
+        fi
         /sbin/ebtables -t nat --list
     fi
 }
