f490623455c002e70e3690af3ca58c83cdd0b4fa
Jack Francis <jackfrancis@gmail.com>
2018-11-05 11:26:01 -0800

    updated VHDs for aks and aks-docker-engine distros (#4178)

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -138,9 +138,9 @@ var (
 	// DefaultAKSOSImageConfig is the AKS image based on Ubuntu 16.04.
 	DefaultAKSOSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
-		ImageSku:       "aks-ubuntu-1604-201810",
+		ImageSku:       "aks-ubuntu-1604-201811",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.10.26",
+		ImageVersion:   "2018.11.02",
 	}
 
 	// DefaultAKSDockerEngineOSImageConfig is the AKS image based on Ubuntu 16.04.
@@ -148,7 +148,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-docker-engine",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.10.26",
+		ImageVersion:   "2018.11.02",
 	}
 
 	//DefaultOpenShift39RHELImageConfig is the OpenShift on RHEL distribution.
