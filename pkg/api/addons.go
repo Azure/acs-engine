f3e024e3667d21dd131c5053a0aad32c3c5deef5
rashmy <rashmy@RASHMY-PC2>
2018-11-06 15:56:30 -0800

    update agent version

--- pkg/api/addons.go
+++ pkg/api/addons.go
@@ -169,7 +169,7 @@ func (cs *ContainerService) setAddonsConfig() {
 		Containers: []KubernetesContainerSpec{
 			{
 				Name:           "omsagent",
-				Image:          "microsoft/oms:containerinventory-cifeature-test-2",
+				Image:          "microsoft/oms:containerinventory-cifeature-test-3",
 				CPURequests:    "50m",
 				MemoryRequests: "200Mi",
 				CPULimits:      "150m",
