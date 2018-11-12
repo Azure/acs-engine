79d8e545b9e15fe144ff63701f14dc5be0e521de
Cecile Robert-Michon <cerobert@@microsoft.com>
2018-11-12 13:59:41 -0800

    adapt WindowsVMNameParts

--- pkg/operations/kubernetesupgrade/upgradecluster.go
+++ pkg/operations/kubernetesupgrade/upgradecluster.go
@@ -338,10 +338,11 @@ func (uc *UpgradeCluster) addVMToAgentPool(vm compute.VirtualMachine, isUpgradab
 	}
 
 	if vm.StorageProfile.OsDisk.OsType == compute.Windows {
-		poolPrefix, _, _, _, err := utils.WindowsVMNameParts(*vm.Name)
-		if err != nil {
-			uc.Logger.Errorf(err.Error())
-			return err
+		poolPrefix, _, _, _, err = utils.WindowsVMNameParts(*vm.Name)
+		if !strings.Contains(uc.NameSuffix, poolPrefix) {
+			uc.Logger.Infof("Skipping VM: %s for upgrade as it does not belong to cluster with expected name suffix: %s\n",
+				*vm.Name, uc.NameSuffix)
+			return nil
 		}
 
 		// The k8s Windows VM Naming Format was previously "^([a-fA-F0-9]{5})([0-9a-zA-Z]{3})([a-zA-Z0-9]{4,6})$" (i.e.: 50621k8s9000)
@@ -351,12 +352,6 @@ func (uc *UpgradeCluster) addVMToAgentPool(vm compute.VirtualMachine, isUpgradab
 			poolIdentifier = (*vm.Name)[:11]
 		} else {
 			poolIdentifier = (*vm.Name)[:9]
-		}
-
-		if !strings.Contains(uc.NameSuffix, poolPrefix) {
-			uc.Logger.Infof("Skipping VM: %s for upgrade as it does not belong to cluster with expected name suffix: %s\n",
-				*vm.Name, uc.NameSuffix)
-			return nil
 		}
 	} else { // vm.StorageProfile.OsDisk.OsType == compute.Linux
 		poolIdentifier, poolPrefix, _, err = utils.K8sLinuxVMNameParts(*vm.Name)
