bdff122e2e5c31c714951f52807dc8e2b6d6602f
Cecile Robert-Michon <cerobert@@microsoft.com>
2018-11-13 18:28:49 -0800

    image sku to 18.04

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -98,7 +98,7 @@ var (
 	//DefaultUbuntuImageConfig is the default Linux distribution.
 	DefaultUbuntuImageConfig = AzureOSImageConfig{
 		ImageOffer:     "UbuntuServer",
-		ImageSku:       "16.04-LTS",
+		ImageSku:       "18.04-LTS",
 		ImagePublisher: "Canonical",
 		ImageVersion:   "latest",
 	}
