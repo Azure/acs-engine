20f33e87c7c301beb0dbcee32128e0789a62d8e6
Jack Francis <jack.francis@microsoft.com>
2018-11-21 08:43:14 -0800

    prop doesn’t exist

--- test/e2e/engine/template.go
+++ test/e2e/engine/template.go
@@ -183,8 +183,8 @@ func Build(cfg *config.Config, masterSubnetID string, agentSubnetID string, isVM
 	}
 
 	if config.EnableKMSEncryption && config.ClientObjectID != "" {
-		if prop.OrchestratorProfile.KubernetesConfig == nil {
-			prop.OrchestratorProfile.KubernetesConfig = &vlabs.KubernetesConfig{}
+		if cs.ContainerService.Properties.OrchestratorProfile.KubernetesConfig == nil {
+			cs.ContainerService.Properties.OrchestratorProfile.KubernetesConfig = &vlabs.KubernetesConfig{}
 		}
 		cs.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.EnableEncryptionWithExternalKms = &config.EnableKMSEncryption
 		cs.ContainerService.Properties.ServicePrincipalProfile.ObjectID = config.ClientObjectID
