2f3e25b3a1537dbfa2d9e75cdfaad7171f22fd48
Patrick Lang <plang@microsoft.com>
2018-11-26 11:40:01 -0800

    Switch to Azure CNI v1.0.14 for Windows

--- pkg/api/const.go
+++ pkg/api/const.go
@@ -246,7 +246,7 @@ const (
 	// AzureCniPluginVerWindows specifies version of Azure CNI plugin, which has been mirrored from
 	// https://github.com/Azure/azure-container-networking/releases/download/${AZURE_PLUGIN_VER}/azure-vnet-cni-windows-amd64-${AZURE_PLUGIN_VER}.tgz
 	// to https://acs-mirror.azureedge.net/cni
-	AzureCniPluginVerWindows = "v1.0.13"
+	AzureCniPluginVerWindows = "v1.0.14"
 	// CNIPluginVer specifies the version of CNI implementation
 	// https://github.com/containernetworking/plugins
 	CNIPluginVer = "v0.7.1"
