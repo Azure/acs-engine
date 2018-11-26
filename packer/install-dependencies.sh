6b4d47913cd3652637366eb6c57e7800eaf29159
Matt Boersma <Matt.Boersma@microsoft.com>
2018-11-26 10:27:59 -0700

    Add support for Kubernetes 1.10.11
    See https://github.com/kubernetes/kubernetes/releases/tag/v1.10.11

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -154,7 +154,7 @@ done
 pullContainerImage "docker" "busybox"
 
 # TODO: fetch supported k8s versions from an acs-engine command instead of hardcoding them here
-K8S_VERSIONS="1.7.15 1.7.16 1.8.14 1.8.15 1.9.10 1.9.11 1.10.8 1.10.9 1.11.4 1.11.5 1.12.1 1.12.2"
+K8S_VERSIONS="1.7.15 1.7.16 1.8.14 1.8.15 1.9.10 1.9.11 1.10.9 1.10.11 1.11.3 1.11.4 1.12.1 1.12.2"
 
 for KUBERNETES_VERSION in ${K8S_VERSIONS}; do
     HYPERKUBE_URL="k8s.gcr.io/hyperkube-amd64:v${KUBERNETES_VERSION}"
