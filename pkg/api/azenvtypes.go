fac2b73165e7be90a4f87a2846f4f6e0231435d7
Jack Francis <jackfrancis@gmail.com>
2018-12-17 10:16:53 -0800

    Enable Azure CNI 1.0.15 (#4361)
    # Conflicts:
    #	pkg/api/const.go

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -140,7 +140,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-201812",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.12.03",
+		ImageVersion:   "2018.12.12",
 	}
 
 	// DefaultAKSDockerEngineOSImageConfig is the AKS image based on Ubuntu 16.04.
@@ -148,7 +148,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-docker-engine",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2018.12.03",
+		ImageVersion:   "2018.12.12",
 	}
 
 	//DefaultOpenShift39RHELImageConfig is the OpenShift on RHEL distribution.
